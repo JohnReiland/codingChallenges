f688dd846453971268124ba15f5f7d66d1edab1b
john <reiland.shanghai@gmail.com>
2020-02-08 14:43:48 -0500

    "refactor: return 0 on 32-bit signed int overflow"

--- reverseInteger.js
+++ reverseInteger.js
@@ -25,10 +25,11 @@ var reverse = (int) => {
   let value;
   while (input !== 0) {
     value = input % 10;
-    result *= 10;
-    result += value;
-    input -= value;
-    input /= 10;
+    result = (result * 10) + value;
+    input = (input - value) / 10;
+  }
+  if (result > 2147483647 || result < -2147483648) {
+    result = 0;
   }
   return result;
 };
