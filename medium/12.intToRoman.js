552d30abcffbbaa39e66c11a11c81ef066b36961
john <reiland.shanghai@gmail.com>
2020-03-25 10:18:12 -0400

    "Refactor out work variable"

--- medium/12.intToRoman.js
+++ medium/12.intToRoman.js
@@ -93,64 +93,61 @@ var intToRoman = (num) => {
  */
 var intToRoman = (num) => {
   let result = '';
-  let $num = num;
 
-  while ($num >= 1000) {
+  while (num >= 1000) {
     result += 'M';
-    $num -= 1000;
+    num -= 1000;
   }
-  if ($num >= 900) {
+  if (num >= 900) {
     result += 'CM';
-    $num -= 900;
+    num -= 900;
   }
-  if ($num >= 500) {
+  if (num >= 500) {
     result += 'D';
-    $num -= 500;
+    num -= 500;
   }
-  if ($num >= 400) {
+  if (num >= 400) {
     result += 'CD';
-    $num -= 400;
+    num -= 400;
   }
 
-  while ($num >= 100) {
+  while (num >= 100) {
     result += 'C';
-    $num -= 100;
+    num -= 100;
   }
-  if ($num >= 90) {
+  if (num >= 90) {
     result += 'XC';
-    $num -= 90;
+    num -= 90;
   }
-  if ($num >= 50) {
+  if (num >= 50) {
     result += 'L';
-    $num -= 50;
+    num -= 50;
   }
-  if ($num >= 40) {
+  if (num >= 40) {
     result += 'XL';
-    $num -= 40;
+    num -= 40;
   }
 
-
-  while ($num >= 10) {
+  while (num >= 10) {
     result += 'X';
-    $num -= 10;
+    num -= 10;
   }
-  if ($num >= 9) {
+  if (num >= 9) {
     result += 'IX';
-    $num -= 9;
+    num -= 9;
   }
-  if ($num >= 5) {
+  if (num >= 5) {
     result += 'V';
-    $num -= 5;
+    num -= 5;
   }
-  if ($num >= 4) {
+  if (num >= 4) {
     result += 'IV';
-    $num -= 4;
+    num -= 4;
   }
 
-
-  while ($num >= 1) {
+  while (num >= 1) {
     result += 'I';
-    $num--;
+    num--;
   }
 
   return result;
