cf536687a39129b592009f0bde82449de8353264
john <reiland.shanghai@gmail.com>
2020-03-30 23:37:10 -0400

    "Complete solution for threeSumClosest"

--- medium/16.threeSumClosest.js
+++ medium/16.threeSumClosest.js
@@ -16,4 +16,45 @@ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  */
 var threeSumClosest = function(nums, target) {
 
+  let array = nums.sort((a, b) => {return a - b});
+
+  if (array.length < 3) {
+    return (array) => array.reduce((a, b) => a + b, 0);
+  }
+
+  let testValue = array[0] + array[1] + array[2];
+  let bestDistance = Math.abs(target - testValue);
+  let result = testValue;
+
+  for (let i = 0; i < array.length - 2; i++) {
+    if (array[i] === array[i - 1]) {
+      continue;
+    }
+    let j = i + 1;
+    let k = array.length - 1;
+    while (j < k) {
+      testValue = array[i] + array[j] + array[k];
+      testDistance = Math.abs(target - testValue);
+      if (testDistance < bestDistance) {
+        bestDistance = testDistance;
+        result = testValue;
+      }
+      if (testValue > target) {
+        k--;
+        while (array[k] === array[k + 1]) {
+          k--;
+        }
+      } else if (testValue < target) {
+        j++;
+        while (array[j] === array[j - 1]) {
+          j++;
+        }
+      } else {
+        return target;
+      }
+    }
+  }
+
+  return result;
+
 };
