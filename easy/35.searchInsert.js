1dfa3e2f63271348f122619aa7dbd807b418798d
john <reiland.shanghai@gmail.com>
2020-05-19 19:46:02 -0400

    "Improved solution for searchInsert"

--- easy/35.searchInsert.js
+++ easy/35.searchInsert.js
@@ -25,7 +25,6 @@ Output: 0
  */
 
 /*
-
 Brute Force Solution:
 
 for each element of sorted array,
@@ -41,7 +40,70 @@ let searchInsert = (nums, target) => {
   }
   return nums.length;
 };
+*/
+
+/*
+Improved Solution:
+
+Use a binary search of sorted array to
+find the index where the target is, or should be
+
+let left variable be 0
+let right variable be array length - 1
+
+if (array[left] > target) {
+  return 0;
+}
+
+if (array[right] < target) {
+  return array length;
+]
+
+let m
+while (l is less than or equal to r) {
+  m is the floor of (l + r) / 2;
+  if (array[m] > target) {
+    r is m - 1;
+  } else if (array[m] < target) {
+    l is m + 1;
+  } else {
+    return m
+  }
+}
+return m;
 
 */
 
-let searchInsert = (nums, target) =>
+let searchInsert = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let middle;
+
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  if (nums[left] >= target) {
+    return 0;
+  }
+
+  if (nums[right] < target) {
+    return nums.length;
+  }
+
+  while (left < right) {
+    middle = Math.floor((left + right)/2);
+    if (nums[middle] > target) {
+      right = middle - 1;
+    } else if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      return middle;
+    }
+  }
+  if (nums[left] >= target) {
+    return left;
+  } else {
+    return left + 1;
+  }
+}
