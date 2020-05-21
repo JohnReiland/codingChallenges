f6ad79678ba394e1340f3740e6601118bfa9d369
john <reiland.shanghai@gmail.com>
2020-05-21 09:26:46 -0400

    "Add naive solution for maxSubArray"

--- easy/53.maxSubArray.js
+++ easy/53.maxSubArray.js
@@ -11,6 +11,34 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
 
-};
+/*
+Naive Solution:
+
+initialize a "current" value at 0
+initialize a "highest" value at 0
+
+for each value in the array
+  add value to current
+  if current is less than zero
+    current is zero
+  else if current is higher than highest
+    highest is current
+return highest
+
+*/
+let maxSubArray = (nums) => {
+  let current = 0;
+  let highest = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    current += nums[i];
+    if (current > highest) {
+      highest = current;
+    }
+    if (current < 0) {
+      current = 0;
+    }
+  }
+  return highest;
+  };
