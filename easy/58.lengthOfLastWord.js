615b4629d33971217fcb259a1c89d6db02bb014b
John <reiland.shanghai@gmail.com>
2020-05-26 09:27:54 -0500

    "Naive solution for lengthOfLastWord"

--- easy/58.lengthOfLastWord.js
+++ easy/58.lengthOfLastWord.js
@@ -14,6 +14,48 @@ Output: 5
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    
+
+ /*
+Naive solution:
+
+if string length === 0
+  return 0
+
+working back from the end of the string:
+  find the index of the first non-space char
+  if there is no non-space char
+    return 0
+  let result = 1
+  for each character prior to that character
+    if that char is non-space
+      result++
+    else
+      return result
+  return result
+
+
+ */
+var lengthOfLastWord = (string) => {
+  if (string.length === 0) {
+    return 0;
+  }
+  let last;
+  for (let i = string.length - 1; i >= 0 && last === undefined; i--) {
+    if (string[i] !== ' ') {
+      last = i;
+    } 
+  }
+  if (last === undefined) {
+    return 0;
+  }
+
+  let result = 1;
+  for (let i = last - 1; i >= 0; i--) {
+    if (string[i] !== ' ') {
+      result++;
+    } else {
+      return result;
+    }
+  }
+  return result;
 };
