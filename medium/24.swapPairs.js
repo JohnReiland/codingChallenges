a646687510f4ffa3dcf01846c917c6c21853a12c
John <reiland.shanghai@gmail.com>
2020-05-28 09:13:40 -0500

    "Initial commit for swapPairs"

---
+++ medium/24.swapPairs.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/*
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may *not* modify the values in the list's nodes, only nodes itself may be changed.
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    
+};
