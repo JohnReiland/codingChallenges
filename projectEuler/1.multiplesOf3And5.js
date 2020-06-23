e8cb2f94cbdf96c9022eb7ebe68102c7c4d27196
john <reiland.shanghai@gmail.com>
2020-06-23 10:40:28 -0500

    "Add brute force solution for projectEuler 1"

--- projectEuler/1.multiplesOf3And5.js
+++ projectEuler/1.multiplesOf3And5.js
@@ -5,3 +5,29 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 
 */
+
+/*
+I can see a brute force approach, and a higher approach.
+
+The brute force approach considers every value between 1 and 1000.
+For each value, if the value is a multiple of 3 OR 5,
+  the value is added to a running total.
+
+The total is then returned.
+
+
+The higher approach uses math insights to jump directly to the solution.
+
+*/
+
+let summedMultiplesOf3And5 = (number) => {
+  let result = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      result += i;
+    }
+  }
+
+  return result;
+}
