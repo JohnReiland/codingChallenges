c9ab1f761e57df766ecc51b501922800665ea567
john <reiland.shanghai@gmail.com>
2020-07-03 22:31:34 -0400

    "Add higher solution for projectEuler 6"

--- projectEuler/6.sumSquareDifference.js
+++ projectEuler/6.sumSquareDifference.js
@@ -79,6 +79,86 @@ let sumSquareDifference = (num) => {
   return squareOfSums(num) - sumOfSquares(num);
 }
 
+sumSquareDifference(4);
+>70
+
+sumSquareDifference(10);
+>2640
+
+sumSquareDifference(100);
+>25164150
+*/
+
+/*
+I wonder if there might be a fast solution for sumOfSquares just as there is for squareOfSums.
+It would involve raising numbers to some powers and rooting them by some base, I assume. Let's
+see if I can detect some kind of pattern by looking at the solutions for the first 10 natural numbers:
+
+sumOfSquares(1);
+>1
+
+sumOfSquares(2);
+>5
+
+sumOfSquares(3);
+>14
+
+sumOfSquares(4);
+>30
+
+sumOfSquares(5);
+>55
+
+sumOfSquares(6);
+>91
+
+sumOfSquares(7);
+>140
+
+sumOfSquares(8);
+>204
+
+sumOfSquares(9);
+>285
+
+sumOfSquares(10);
+>385
+
+Nothing immedieatly jumps out.
+EDIT: after online research I've learned the name for the numbers I'm looking for:
+Square pyramidal numbers.
+The formula for calculating the nth pyramidal number is:
+(n * (n + 1) * (2n + 1)) / 6
+
+I think it's safe to say I wouldn't quickly figure this formula out on my own.
+
+Updating sumOfSquares to use this method:
+
+let sumOfSquares = (num) => {
+  return ((num) * (num + 1) * ((2 * num) + 1)) / 6;
+}
+
+sumOfSquares(2);
+>5
+
+sumOfSquares(10);
+>385
+
+sumOfSquares(100);
+>338350
+
+Updating sumSquareDifferenceL
+
+let sumSquareDifference = (num) => {
+  let sumOfSquares = (num) => {
+  return ((num) * (num + 1) * ((2 * num) + 1)) / 6;
+  }
+  let squareOfSums = (num) => {
+    let sum = (num * (num + 1)) / 2;
+    return sum * sum;
+  }
+  return squareOfSums(num) - sumOfSquares(num);
+}
 
 sumSquareDifference(4);
 >70
