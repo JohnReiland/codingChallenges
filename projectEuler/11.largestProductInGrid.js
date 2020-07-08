e65a2f3d48e3d3fa2251597b22a32ab61228cc75
john <reiland.shanghai@gmail.com>
2020-07-08 14:56:16 -0400

    "Add grid convert, horiz-test for projectEuler 11"

--- projectEuler/11.largestProductInGrid.js
+++ projectEuler/11.largestProductInGrid.js
@@ -26,3 +26,148 @@ The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 
 What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 */
+
+/*
+There's considerable ambiguity in the input of this challenge
+Is the grid a string? An array? An array of arrays? I see values
+with leading zeros, that's unusual. Zero is represented by double zero,
+likewise unusual.
+
+If I were designing the inputs, I would use an array of arrays,
+but I see nothing in the problem to suggest that's the input here,
+and the leading zeros actually suggest against it. I'm going to
+assume the input is one long string.
+
+While I'm making assumptions, I'm going to assume the size of the
+grid might vary, but will always be square. This grid is 20x20.
+I'm not going to assume all grids will be 20x20, but I'm not going
+to consider the possibilty of a 10x30 grid. 5x5, yes. 8x8, yes.
+
+I'm also going to assume that the number of adjacent numbers
+considered also varies. The entire width of the grid will be the
+default, but smaller values, as small as 1, can be passed in.
+
+For the output, I want not only the product, but the factors as well.
+I'll output the result as: [[...factors], product]
+*/
+
+/*
+Strategy
+I built a connect-four game once that had to test, after every move,
+whether a player had one. Like this challenge, it required testing
+the same grid-like game board in multiple ways. I used seperate functions
+to test for different kinds of wins, vertical horizontal, major diagonal,
+minor diagonal. I'll do the same here, but instead of testing for four
+alike values, I'll multiply the values and test whether the product is
+the highest found for that test. Each test will return the factors and
+product that are the highest it found. A cross-test comparisson will
+find the highest among all tests.
+
+So first there will be a helper function to convert the string into an array of arrays for easy manipulation, then a series of helper functions which each return the highest product they could find using the given (or default) adjacency and the factors of that product, and then the highest among those is returned as the result.
+*/
+
+let largestProductInGrid = (string, adjacency) => {
+  let populateGrid = () => {
+    let currentValue = 0;
+    for (let i = 0; i < size; i++) {
+      grid.push([]);
+    }
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        grid[i][j] = parseInt(gridString[currentValue], 10);
+        currentValue++;
+      }
+    }
+  }
+
+  let largestHorizontalProduct = (grid, adjacency) => {
+    let result = [[], 0];
+    let currentProduct;
+    for (let i = 0; i < grid.length; i++) {
+      currentProduct = 1;
+      for (let j = 0; j < adjacency; j++) {
+        currentProduct *= grid[i][j];
+      }
+      if (result[1] < currentProduct) {
+        result = [[], currentProduct];
+        for (let k = 0; k < adjacency; k++) {
+          result[0].push(grid[i][k]);
+        }
+      }
+
+      for (let j = adjacency + 1; j < grid.length; j++) {
+        currentProduct /= grid[i][j - 1];
+        currentProduct *= grid[i][j];
+        if (result[1] < currentProduct) {
+          result = [[], currentProduct];
+          for (let k = j - adjacency; k < j; k++) {
+            result[0].push(grid[i][k]);
+          }
+        }
+      }
+
+    }
+    return result;
+  }
+
+  let largestVerticalProduct = (grid, adjacency) => {
+    let result = [[], 0];
+
+    return result;
+  }
+
+  let largestMajorDiagonalProduct = (grid, adjacency) => {
+    let result = [[], 0];
+    return result;
+  }
+
+  let largestMinorDiagonalProduct = (grid, adjacency) => {
+    let result = [[], 0];
+    return result;
+  }
+
+
+  // MAIN EXECUTION START
+
+  let gridString = string.split(' ');
+  let grid = [];
+  let size = Math.sqrt(gridString.length);
+  adjacency = adjacency || size;
+  if (adjacency > size || adjacency < 1) {
+    adjacency = size;
+  }
+  populateGrid();
+  //let vertical = largestVerticalProduct(grid, adjacency);
+  let horizontal = largestHorizontalProduct(grid, adjacency);
+  //let majorDiagonal = largestMajorDiagonalProduct(grid, adjacency);
+  //let minorDiagonal = largestMinorDiagonalProduct(grid, adjacency);
+
+  let largest = horizontal;
+
+  /*
+  if (vertical[1] > largest[1]) {
+    largest = vertical;
+  }
+  if (majorDiagonal[1] > largest[1]) {
+    largest = majorDiagonal;
+  }
+  if (minorDiagonal[1] > largest[1]) {
+    largest = minorDiagonal;
+  }
+  */
+
+  let result = [[]];
+  result.push(largest[1]);
+  for (let i = 0; i < largest[0].length; i++) {
+    if (largest[0][i] < 10) {
+      result[0].push('0' + largest[0][i].toString());
+    } else {
+      result[0].push(largest[0][i].toString());
+    }
+  }
+
+  return result;
+}
+
+
+module.exports = largestProductInGrid;
---
+++ projectEuler/tests/11.largestProductInGrid.test.js
@@ -0,0 +1,44 @@
+const largestProductInGrid = require('../11.largestProductInGrid');
+
+/*
+test('Given a string able to construct a 2x2 grid, output 2', () => {
+  expect(largestProductInGrid('01 01 01 01')).toBe(2);
+})
+
+test('Given a string able to construct a 3x3 grid, output 3', () => {
+  expect(largestProductInGrid('01 01 01 01 01 01 01 01 01')).toBe(3);
+})
+
+test('Given a string able to construct a 5x5 grid, output 5', () => {
+  expect(largestProductInGrid('01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01')).toBe(5);
+})
+
+*/
+
+test('Existential: function "largestProductInGrid" should exist', () => {
+  expect(largestProductInGrid('01 01 01 01')).toBeDefined();
+})
+
+test('Functional: Given a 2x2 grid of "01 01 01 01", output should be [["01", "01"], 1]', () => {
+  expect(largestProductInGrid('01 01 01 01')).toStrictEqual([['01', '01'], 1]);
+})
+
+test('Horizontal: Given a 2x2 grid of "02 02 01 01", output should be [["02", "02"], 4]', () => {
+  expect(largestProductInGrid('02 02 01 01')).toStrictEqual([['02', '02'], 4]);
+})
+
+/*
+
+test('Vertical: Given a 2x2 grid of "02 01 02 01", output should be [["02", "02"], 4]', () => {
+  expect(largestProductInGrid('02 01 02 01')).toStrictEqual([['02', '02'], 4]);
+})
+
+test('Major Diagonal: Given a 2x2 grid of "02 01 01 02", output should be [["02", "02"], 4]', () => {
+  expect(largestProductInGrid('02 01 01 02')).toStrictEqual([['02', '02'], 4]);
+})
+
+test('Minor Diagonal: Given a 2x2 grid of "01 02 02 01", output should be [["02", "02"], 4]', () => {
+  expect(largestProductInGrid('01 02 02 01')).toStrictEqual([['02', '02'], 4]);
+})
+
+*/
