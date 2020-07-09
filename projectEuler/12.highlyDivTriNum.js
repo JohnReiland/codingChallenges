6e0eb303d4a3a720699a6e716091d67128f87e60
john <reiland.shanghai@gmail.com>
2020-07-09 14:03:12 -0400

    "Add brute force method for projectEuler 12"

--- projectEuler/12.highlyDivTriNum.js
+++ projectEuler/12.highlyDivTriNum.js
@@ -26,7 +26,13 @@ one until one with 501 divisors is found.
 
 There should be a fast solution, and perhaps it can be found by analizing the divisors
 not only of triangle numbers, but of all numbers. I'll build a brute force solution
-first and then come back for the fast solution
+first and then come back for the fast solution.
+
+UPDATE:
+As suspected, the brute force method is effective for small target values (less than
+200), but takes ages to calculate for target quantities of divisors much higher.
+Finding the triangle number with more than 400 divisors, on my machine, took over 3 minutes, and finding the triangle number with more than 500 divisors took almost 12 minutes. A faster method is needed.
+
 */
 
 /*
@@ -72,4 +78,36 @@ let divisors = (num) => {
   return result;
 }
 
-module.exports = {divisors};
+let highlyDivTriNum = (target) => {
+  let triangles = [1];
+  while (divisors(triangles[triangles.length - 1]).length <= target) {
+    findNextTriangle(triangles);
+  }
+  return triangles[triangles.length - 1];
+}
+
+/*
+highlyDivTriNum(2);
+>6
+
+highlyDivTriNum(5);
+>28
+
+highlyDivTriNum(100);
+>73920
+
+highlyDivTriNum(200);
+>2031120
+
+highlyDivTriNum(300);
+>2162160
+
+highlyDivTriNum(400);
+>17907120
+
+highlyDivTriNum(500);
+>76576500
+
+*/
+
+module.exports = {findNextTriangle, divisors, highlyDivTriNum};
--- projectEuler/tests/12.highlyDivTriNum.test.js
+++ projectEuler/tests/12.highlyDivTriNum.test.js
@@ -1,29 +1,58 @@
-const {findNextTriangle, divisors} = require('../12.highlyDivTriNum');
+const {findNextTriangle, divisors, highlyDivTriNum} = require('../12.highlyDivTriNum');
 
-test('divisors(1) should return [1]', () => {
-  expect(divisors(1)).toStrictEqual([1]);
-});
 
-test('divisors(3) should return [1, 3]', () => {
-  expect(divisors(3)).toStrictEqual([1, 3]);
-});
+describe('divisors()', () => {
 
-test('divisors(6) should return [1, 2, 3, 6]', () => {
-  expect(divisors(6)).toStrictEqual([1, 2, 3, 6]);
-});
+  test('divisors(1) should return [1]', () => {
+    expect(divisors(1)).toStrictEqual([1]);
+  });
 
-test('divisors(10) should return [1, 2, 5, 10]', () => {
-  expect(divisors(10)).toStrictEqual([1, 2, 5, 10]);
-});
+  test('divisors(3) should return [1, 3]', () => {
+    expect(divisors(3)).toStrictEqual([1, 3]);
+  });
 
-test('divisors(15) should return [1, 3, 5, 15]', () => {
-  expect(divisors(15)).toStrictEqual([1, 3, 5, 15]);
-});
+  test('divisors(6) should return [1, 2, 3, 6]', () => {
+    expect(divisors(6)).toStrictEqual([1, 2, 3, 6]);
+  });
 
-test('divisors(21) should return [1, 3, 7, 21]', () => {
-  expect(divisors(21)).toStrictEqual([1, 3, 7, 21]);
-});
+  test('divisors(10) should return [1, 2, 5, 10]', () => {
+    expect(divisors(10)).toStrictEqual([1, 2, 5, 10]);
+  });
 
-test('divisors(28) should return [1, 2, 4, 7, 14, 28]', () => {
-  expect(divisors(28)).toStrictEqual([1, 2, 4, 7, 14, 28]);
-});
+  test('divisors(15) should return [1, 3, 5, 15]', () => {
+    expect(divisors(15)).toStrictEqual([1, 3, 5, 15]);
+  });
+
+  test('divisors(21) should return [1, 3, 7, 21]', () => {
+    expect(divisors(21)).toStrictEqual([1, 3, 7, 21]);
+  });
+
+  test('divisors(28) should return [1, 2, 4, 7, 14, 28]', () => {
+    expect(divisors(28)).toStrictEqual([1, 2, 4, 7, 14, 28]);
+  });
+
+})
+
+describe('highlyDivTriNum()', () => {
+
+  test('highlyDivTriNum(1) should be 3', () => {
+    expect(highlyDivTriNum(1)).toBe(3);
+  })
+
+  test('highlyDivTriNum(2) should be 6', () => {
+    expect(highlyDivTriNum(2)).toBe(6);
+  })
+
+  test('highlyDivTriNum(3) should be 6', () => {
+    expect(highlyDivTriNum(3)).toBe(6);
+  })
+
+  test('highlyDivTriNum(4) should be 28', () => {
+    expect(highlyDivTriNum(4)).toBe(28);
+  })
+
+  test('highlyDivTriNum(5) should be 28', () => {
+    expect(highlyDivTriNum(5)).toBe(28);
+  })
+
+})
