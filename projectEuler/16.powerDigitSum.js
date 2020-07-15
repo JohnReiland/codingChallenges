f1c91d686a2bd2271f9ca25e9dc446e3fb332c84
john <reiland.shanghai@gmail.com>
2020-07-15 12:35:45 -0400

    "Initial commit for projectEuler 16"

---
+++ projectEuler/16.powerDigitSum.js
@@ -0,0 +1,11 @@
+/*
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+*/
+
+let powerDigitSum = (base, power) => {
+
+}
+
+module.export = {powerDigitSum};
---
+++ projectEuler/tests/16.powerDigitSum.test.js
@@ -0,0 +1,9 @@
+const {powerdigitSum} = require('../15.powerDigitSum');
+
+describe('testName()', () => {
+
+  test('testName(value) should be result', () => {
+    expect(testName(value)).toBe(result);
+  })
+
+})
