89be74e1d68597bf8e65caf21e6e68e64b7d5837
john <reiland.shanghai@gmail.com>
2020-07-19 19:05:19 -0400

    "Add chart[] to projectEuler 17"

--- projectEuler/17.numberLetterCounts.js
+++ projectEuler/17.numberLetterCounts.js
@@ -14,10 +14,62 @@ whole numbers from fractions (ex: one and four fifths), and because of this, the
 way of saying 115 (and so the way of writing out 115) that feels "right" to me is
 "one hundred fifteen." But putting that aside, this shouldn't be too difficult.
 I should be able to make an object literal to store the letter-values for the numbers
-"one" through "nineteen," then "twenty" through "ninety," and then just "hundred," "thousand, and "and."
+"one" through "nineteen," then "twenty" through "ninety," and then just "hundred,"
+"thousand, and "and."
 
 I'll make the counting function accept values for start and end, inclusive, and a
-helper function to return the letter value for just one number. The counting function will just pass all numbers from and inclusind the start value to and including the
+helper function to return the letter value for just one number. The counting function
+will just pass all numbers from and inclusind the start value to and including the
 end value, summing the returns as it does so, and outputing the final sum.
+
+As a bonus, I think I'll include a boolean parameter to output the sum for American
+usage (and ommission) of "and".
+
+Main execution should look something like:
+
+numberLetterCount(1, 5);
+>19
 */
 
+let chart = {
+0: ["zero", 4],
+1: ["one", 3],
+2: ["two", 3],
+3: ["three", 5],
+4: ["four", 4],
+5: ["five", 4],
+6: ["six", 3],
+7: ["seven", 5],
+8: ["eight", 5],
+9: ["nine", 4],
+10: ["ten", 3],
+11: ["eleven", 6],
+12: ["twelve", 6],
+13: ["thirteen", 8],
+14: ["fourteen", 8],
+15: ["fifteen", 7],
+16: ["sixteen", 7],
+17: ["seventeen", 9],
+18: ["eightteen", 9],
+19: ["nineteen", 8],
+20: ["twenty", 6],
+30: ["thirty", 6],
+40: ["fourty", 6],
+50: ["fifty", 5],
+60: ["sixty", 5],
+70: ["seventy", 7],
+80: ["eighty", 6],
+90: ["ninety", 6]
+}
+
+/*
+chart[1][1];
+>3
+
+chart[19][1];
+>8
+*/
+
+
+
+module.exports = {chart, letterCount};
--- projectEuler/tests/17.numberLetterCounts.test.js
+++ projectEuler/tests/17.numberLetterCounts.test.js
@@ -1,9 +1,29 @@
-const numberLetterCount = require('../.17numberLetterCount');
+const {chart, letterCount} = require('../17.numberLetterCounts');
 
-describe('test()', () => {
+describe('chart[]', () => {
 
-  test('test(value) should be result', () => {
-    expect(test(value)).toBe(result);
+  test('chart[1][1] should be 3', () => {
+    expect(chart[1][1]).toBe(3);
+  })
+
+  test('chart[19][1] should be 8', () => {
+    expect(chart[19][1]).toBe(8);
+  })
+
+})
+
+describe('letterCount()', () => {
+
+  test('letterCount(77) should be 13', () => {
+    expect(letterCount(77)).toBe(13)
+  })
+
+  test('letterCount(189) should be 20', () => {
+    expect(letterCount(189)).toBe(20)
+  })
+
+  test('letterCount(810) should be 15', () => {
+    expect(letterCount(810)).toBe(15)
   })
 
 })
