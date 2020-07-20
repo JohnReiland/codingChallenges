3ba6cc3affadbc5ab6f1415b3142c7094cb7bc3f
john <reiland.shanghai@gmail.com>
2020-07-20 16:40:59 -0400

    "Add fast solution for projectEuler 18"

--- projectEuler/tests/18.maxPathSum.test.js
+++ projectEuler/tests/18.maxPathSum.test.js
@@ -1,4 +1,4 @@
-const {collapseRow} =  require('../18.maxPathSum');
+const {collapseRow, maxPathSum} =  require('../18.maxPathSum');
 
 describe('collapseRow()', () => {
 
@@ -8,6 +8,18 @@ describe('collapseRow()', () => {
 
   test('collapseRow([1, 2, 3, 4], [5, 6, 7, 8, 9]) shoule be [7, 9, 11, 13]', () => {
     expect(collapseRow([1, 2, 3, 4], [5, 6, 7, 8, 9])).toStrictEqual([7, 9, 11, 13]);
+  })
+
+})
+
+describe('maxPathSum()', () => {
+
+  test('maxPathSum([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]) should be 23', () => {
+    expect(maxPathSum([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]])).toBe(23);
+  })
+
+  test('maxPathSum([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3], [6, 4, 8, 2, 1]]) should be 31', () => {
+    expect(maxPathSum([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3], [6, 4, 8, 2, 1]])).toBe(31);
   })
 
 })
