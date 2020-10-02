4e77c8f1e13d778ae1d3adf5bf10e0d66f02b8a9
John Reiland <reiland.shanghai@gmail.com>
2020-10-02 00:26:00 -0400

    "Add working solution for addBinary"

--- easy/67.addBinary.js
+++ easy/67.addBinary.js
@@ -30,8 +30,28 @@ Each string is either "0" or doesn't contain any leading zero.
 I imagine I can easily make helper functions that convert between binary and
 base 10, and then make an addBinary function that makes use of both helper
 functions.
+
+UPDATE: This works for small values, however, long strings convert to base 10
+numbers inaccurately. A bitwise solution is needed.
+
+Looking at examples of binary numbers added using the naive solution, a method
+seems staightforward. Calculation begins with the least significant digit of
+each string.
+If both are 0, their sum is 0, and calculation continues.
+If exactly one is a 0, their sum is 1, and calculation continues.
+If both are 1, their sum is 10. A 0 should be writen with the 1 carrying over
+to the next place. Continue until at least one string is done AND there are no
+more carried 1s, then return the sum.
+
+There are a number of possible states in calculation.
+First, neither string is finished.
+Second, one or both strings are finished, but carried is not empty.
+Third, one or both strings are finished and carried is empty.
+
+These states require consideration when designing the function.
 */
 
+/*
 let toBase10 = (string) => {
   return parseInt(string, 2);
 };
@@ -44,6 +64,102 @@ let addBinary = (a, b) => {
   return toBin(toBase10(a) + toBase10(b));
 };
 
+addBinary("1010", "1011");
+>"10101"
+
+addBinary("11", "1");
+>"101"
+
+*/
+
+let addAndCarry = (top, bottom, carry) => {
+  let result;
+  let newCarry = carry;
+  if (top === bottom) {
+    if (top === "1") {
+      if (newCarry === 0) {
+        result = "0";
+        newCarry++;
+      } else {
+        result = "1";
+      }
+    } else {
+      if (newCarry > 0) {
+        result = "1";
+        newCarry--;
+      } else {
+        result = "0";
+      }
+    }
+  } else {
+    if (newCarry > 0) {
+      result = "0";
+    } else {
+      result = "1";
+    }
+  }
+  return [result, newCarry];
+};
+
+let mergeCarry = (value, carry) => {
+  let result;
+  let newCarry = carry;
+  if (newCarry > 0) {
+    if (value === "0") {
+      result = "1";
+      newCarry--;
+    } else {
+      result = "0";
+    }
+  } else {
+    result = value;
+  }
+  return [result, newCarry];
+};
+
+let addBinary = (a, b) => {
+  if (a === "0" || b === "0") {
+    return a === "0" ? b : a;
+  }
+  let result = "";
+  let index = 0;
+  let carry = 0;
+  while (
+    a[a.length - (1 + index)] !== undefined &&
+    b[b.length - (1 + index)] !== undefined
+  ) {
+    let temp = addAndCarry(
+      a[a.length - (1 + index)],
+      b[b.length - (1 + index)],
+      carry
+    );
+    result = temp[0] + result;
+    carry = temp[1];
+    index++;
+  }
+  while (
+    a[a.length - (1 + index)] !== undefined ||
+    b[b.length - (1 + index)] !== undefined
+  ) {
+    let temp = mergeCarry(
+      a[a.length - (1 + index)] === undefined
+        ? b[b.length - (1 + index)]
+        : a[a.length - (1 + index)],
+      carry
+    );
+    result = temp[0] + result;
+    carry = temp[1];
+    index++;
+  }
+  while (carry > 0) {
+    result = "1" + result;
+    carry--;
+  }
+  return result;
+};
+
 module.exports = {
   addBinary,
+  addAndCarry,
+  mergeCarry,
 };
--- easy/tests/67.addBinary.test.js
+++ easy/tests/67.addBinary.test.js
@@ -1,23 +1,94 @@
-const { addBinary } = require("../67.addBinary");
+const { addBinary, addAndCarry, mergeCarry } = require("../67.addBinary");
+
+describe("addAndCarry()", () => {
+  test("addAndCarry('0', '0', 0) should be ['0', 0]", () => {
+    expect(addAndCarry("0", "0", 0)).toStrictEqual(["0", 0]);
+  });
+
+  test("addAndCarry('0', '1', 0) should be ['0', 0]", () => {
+    expect(addAndCarry("0", "1", 0)).toStrictEqual(["1", 0]);
+  });
+
+  test("addAndCarry('1', '0', 0) should be ['0', 0]", () => {
+    expect(addAndCarry("1", "0", 0)).toStrictEqual(["1", 0]);
+  });
+
+  test("addAndCarry('1', '1', 0) should be ['0', 1]", () => {
+    expect(addAndCarry("1", "1", 0)).toStrictEqual(["0", 1]);
+  });
+
+  test("addAndCarry('0', '0', 1) should be ['1', 0]", () => {
+    expect(addAndCarry("0", "0", 0)).toStrictEqual(["0", 0]);
+  });
+
+  test("addAndCarry('0', '1', 1) should be ['0', 1]", () => {
+    expect(addAndCarry("0", "1", 0)).toStrictEqual(["1", 0]);
+  });
+
+  test("addAndCarry('1', '0', 1) should be ['0', 1]", () => {
+    expect(addAndCarry("1", "0", 0)).toStrictEqual(["1", 0]);
+  });
+
+  test("addAndCarry('1', '1', 1) should be ['0', 2]", () => {
+    expect(addAndCarry("1", "1", 0)).toStrictEqual(["0", 1]);
+  });
+});
+
+describe("mergeCarry()", () => {
+  test("mergeCarry('0', 0) should be ['0', 0]", () => {
+    expect(mergeCarry("0", 0)).toStrictEqual(["0", 0]);
+  });
+
+  test("mergeCarry('1', 0) should be ['1', 0]", () => {
+    expect(mergeCarry("1", 0)).toStrictEqual(["1", 0]);
+  });
+
+  test("mergeCarry('0', 1) should be ['1', 0]", () => {
+    expect(mergeCarry("0", 1)).toStrictEqual(["1", 0]);
+  });
+
+  test("mergeCarry('1', 1) should be ['0', 1]", () => {
+    expect(mergeCarry("1", 1)).toStrictEqual(["0", 1]);
+  });
+});
 
 describe("addBinary()", () => {
-  test("addBinary('1010', '0' should be '1010'", () => {
+  test("addBinary('1010', '0') should be '1010'", () => {
     expect(addBinary("1010", "0")).toBe("1010");
   });
 
-  test("addBinary('0', '1011' should be '1011'", () => {
+  test("addBinary('0', '1011') should be '1011'", () => {
     expect(addBinary("0", "1011")).toBe("1011");
   });
 
-  test("addBinary('0', '0' should be '0'", () => {
+  test("addBinary('0', '0') should be '0'", () => {
     expect(addBinary("0", "0")).toBe("0");
   });
 
-  test("addBinary('11', '1' should be '100'", () => {
+  test("addBinary('11', '1') should be '100'", () => {
     expect(addBinary("11", "1")).toBe("100");
   });
 
-  test("addBinary('1010', '1011' should be '10101'", () => {
+  test("addBinary('101', '101') should be '1010'", () => {
+    expect(addBinary("101", "101")).toBe("1010");
+  });
+
+  test("addBinary('111', '111') should be '1110'", () => {
+    expect(addBinary("111", "111")).toBe("1110");
+  });
+
+  test("addBinary('1010', '1011') should be '10101'", () => {
     expect(addBinary("1010", "1011")).toBe("10101");
+  });
+
+  test("addBinary('10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101', '110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011') should be '110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000'", () => {
+    expect(
+      addBinary(
+        "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+        "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+      )
+    ).toBe(
+      "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000"
+    );
   });
 });
