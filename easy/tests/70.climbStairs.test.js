c4cd8bdaa44cb5be90087b5001cefbce1bf691c8
John Reiland <reiland.shanghai@gmail.com>
2020-10-03 23:26:21 -0400

    "Initial commit for climbStairs"

---
+++ easy/tests/70.climbStairs.test.js
@@ -0,0 +1,16 @@
+const { TestScheduler } = require("jest");
+const { climbStairs } = require("../60.climbStairs");
+
+describe("climbStairs", () => {
+  test("climbstairs(2) should be 2", () => {
+    expect(climbStairs(2)).toBe(2);
+  });
+
+  test("climbstairs(3) should be 3", () => {
+    expect(climbStairs(3)).toBe(3);
+  });
+
+  test("climbstairs(4) should be 5", () => {
+    expect(climbStairs(4)).toBe(5);
+  });
+});
