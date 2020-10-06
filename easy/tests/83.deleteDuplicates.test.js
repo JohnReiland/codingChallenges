37b9b3ec8efa3bda37d86352bf45ea48ba1e5474
John Reiland <reiland.shanghai@gmail.com>
2020-10-05 20:01:30 -0400

    "Initial commit for deleteDuplicates"

---
+++ easy/tests/83.deleteDuplicates.test.js
@@ -0,0 +1,8 @@
+const { TestScheduler } = require("jest");
+const { deleteDuplicates } = require("../83.deleteDuplicates");
+
+describe("deleteDuplicates", () => {
+  test("deleteDuplicates(value) should be result", () => {
+    expect(deleteDuplicates(value)).toBe(result);
+  });
+});
