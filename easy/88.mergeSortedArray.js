7316dea77768ec36a778efcf58bed7b676a3c65b
John Reiland <reiland.shanghai@gmail.com>
2020-10-06 16:28:38 -0400

    "Add naive solution for mergeSortedArray"

--- easy/88.mergeSortedArray.js
+++ easy/88.mergeSortedArray.js
@@ -22,6 +22,42 @@ nums1.length == m + n
 nums2.length == n
 */
 
-let merge = (nums1, m, nums2, n) => {};
+/*
+It's not entirely clear to me the purpose of initializing the first array to be
+a length sufficient to hold the merged arrays. Clearly, this makes returning the
+first array as the result the obvious solution, but it's unclear why that should
+be made the obvious solution. Pure functions are often preferable, and yet the
+challenge seems to advocate for a solution employing an impure function.
+
+Also odd is the constraint of the range of values within the arrays; negative
+one billion to positive one billion. It's not immedieatly clear why this should
+matter.
+
+I think the fastest way to merge this will be in-place. That is, an attempt
+should be made to merge the arrays without changing the length of either array
+at any time. This should be possible with a series of value swaps. ES6 allows
+this to be accomplished without needing a temporary variable.
+
+Despite there being only two arrays, there are, in effect, three places that
+values are being swapped to/from. They are the start of nums1 (index 0 -> m-1),
+the end of nums1 (index m -> nums1.length-1), and nums2.
+*/
+
+let merge = (nums1, m, nums2, n) => {
+  for (let i = 0, j = 0; i < m; i++) {
+    if (nums1[i] > nums2[j]) {
+      [nums1[i], nums2[j]] = [nums2[j], nums1[i]];
+      while (nums2[j] > nums2[j + 1]) {
+        [nums2[j], nums2[j + 1]] = [nums2[j + 1], nums2[j]];
+        j++;
+      }
+      j = 0;
+    }
+  }
+  for (let i = m, j = 0; i < nums1.length; i++, j++) {
+    nums1[i] = nums2[j];
+  }
+  return nums1;
+};
 
 module.exports = { merge };
--- easy/tests/88.mergeSortedArray.test.js
+++ easy/tests/88.mergeSortedArray.test.js
@@ -3,13 +3,24 @@ const { merge } = require("../88.mergeSortedArray");
 
 describe("merge", () => {
   const test1 = [1, 2, 3, 0, 0, 0];
-  const m = 3;
+  const m1 = 3;
   const test2 = [2, 5, 6];
-  const n = 3;
+  const n1 = 3;
 
   const result1 = [1, 2, 2, 3, 5, 6];
 
-  test("merge(test1, m, test2, n) should be result1", () => {
-    expect(merge(test1, m, test2, n)).toEqual(result1);
+  test("merge(test1, m1, test2, n1) should be result1", () => {
+    expect(merge(test1, m1, test2, n1)).toEqual(result1);
+  });
+
+  const test3 = [2, 5, 6, 0, 0, 0];
+  const m2 = 3;
+  const test4 = [1, 2, 3];
+  const n2 = 3;
+
+  const result2 = [1, 2, 2, 3, 5, 6];
+
+  test("merge(test3, m2, test4, n2) should be result1", () => {
+    expect(merge(test3, m2, test4, n2)).toEqual(result2);
   });
 });
