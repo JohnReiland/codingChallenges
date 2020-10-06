f3c571a5eec68bb753b2ab5664b1cd4a71846dd5
John Reiland <reiland.shanghai@gmail.com>
2020-10-05 20:44:01 -0400

    "Add solution for deleteDuplicates"

--- easy/83.deleteDuplicates.js
+++ easy/83.deleteDuplicates.js
@@ -11,6 +11,54 @@ Input: 1->1->2->3->3
 Output: 1->2->3
 */
 
-let deleteDuplicates = (head) => {};
+/*
+This should be dead simple. Have a left and right hand. The right hand takes the
+first node and passes it to the left. Then it takes the next node. Whenever the
+node it takes has the same value as the one in the left hand, it take the next
+node without passing to the left hand. When a node wife a different value is
+found the left hand's next is made the node in the right hand. 
 
-module.exports = { deleteDuplicates };
+When the end of the list is reached, output the head.
+*/
+
+/*
+UPDATE:
+One detail I didn't see in the initial design was the necessity for the next
+value of the last node to be explicitly made null. This is only necessary when
+the last unique value in the list has duplicates, but is absolutely necessary.
+*/
+
+let deleteDuplicates = (head) => {
+  if (!head) {
+    return head;
+  }
+  let left = head;
+  while (left.next !== null) {
+    let right = left.next;
+    while (right && right.val === left.val) {
+      right = right.next;
+    }
+    if (right) {
+      left.next = right;
+      left = left.next;
+    } else {
+      left.next = null;
+    }
+  }
+  return head;
+};
+
+let ListNode = (val, next) => {
+  this.val = val === undefined ? 0 : val;
+  is.next = next === undefined ? null : next;
+};
+
+/*
+deleteDuplicates([1,1,2]);
+>[1,2]
+
+deleteDuplicates([1,1,2,3,3]);
+>[1,2,3]
+*/
+
+module.exports = { deleteDuplicates, ListNode };
--- easy/tests/83.deleteDuplicates.test.js
+++ easy/tests/83.deleteDuplicates.test.js
@@ -1,8 +1,59 @@
 const { TestScheduler } = require("jest");
-const { deleteDuplicates } = require("../83.deleteDuplicates");
+const { deleteDuplicates, ListNode } = require("../83.deleteDuplicates");
 
 describe("deleteDuplicates", () => {
-  test("deleteDuplicates(value) should be result", () => {
-    expect(deleteDuplicates(value)).toBe(result);
+  const test1 = {
+    val: 1,
+    next: {
+      val: 1,
+      next: {
+        val: 2,
+        next: null,
+      },
+    },
+  };
+
+  const result1 = {
+    val: 1,
+    next: {
+      val: 2,
+      next: null,
+    },
+  };
+
+  test("deleteDuplicates(1->1->2) should be 1->2", () => {
+    expect(deleteDuplicates(test1)).toEqual(result1);
+  });
+
+  const test2 = {
+    val: 1,
+    next: {
+      val: 1,
+      next: {
+        val: 2,
+        next: {
+          val: 3,
+          next: {
+            val: 3,
+            next: null,
+          },
+        },
+      },
+    },
+  };
+
+  const result2 = {
+    val: 1,
+    next: {
+      val: 2,
+      next: {
+        val: 3,
+        next: null,
+      },
+    },
+  };
+
+  test("deleteDuplicates(1->1->2) should be 1->2", () => {
+    expect(deleteDuplicates(test2)).toEqual(result2);
   });
 });
