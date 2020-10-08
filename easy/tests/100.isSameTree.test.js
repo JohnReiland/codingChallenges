c04486c8d57a7cc8f668d6ac4343588c31f39689
John Reiland <reiland.shanghai@gmail.com>
2020-10-08 15:01:08 -0400

    "Initial commit for isSameTree"

---
+++ easy/tests/100.isSameTree.test.js
@@ -0,0 +1,7 @@
+const { isSameTree, TreeNode } = require{ '../100.isSameTree'};
+
+describe('isSameTree', () => {
+  test('isSameTree(value) should be result', () => {
+    expect(isSameTree(value)).toStrictEqual(result);
+  })
+})
