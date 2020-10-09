1e2cd8678cbabdc7b10074db183118963c11accc
John Reiland <reiland.shanghai@gmail.com>
2020-10-08 20:14:14 -0400

    "Add object-oriented solution for isSameTree"

--- easy/100.isSameTree.js
+++ easy/100.isSameTree.js
@@ -27,14 +27,69 @@ Input:     1         1
 
         [1,2,1],   [1,1,2]
 Output: false
+
+It seems to me that the obvious helper funtion takes a TreeNode and returns an
+array of its values just as the shorthand appears in the example. These arrays
+can then be compared to each other, and if they are deeply alike, then the
+binary trees can be said to be the same, as defined by this challenge.
+
+This will necessitate creating a helper function that can deeply compare arrays.
+
+Final form of the function will be something like:
+
+let isSameTree = (tree1, tree2) => {
+  return arraysAreAlike(
+    treeToArray(tree1),
+    treeToArray(tree2)
+  );
+};
 */
 
-let TreeNode = (val, left, right) => {
+function TreeNode(val, left, right) {
   this.val = val === undefined ? 0 : val;
   this.left = left === undefined ? null : left;
   this.right = right === undefined ? null : right;
+}
+
+let arraysAreAlike = (array1, array2) => {
+  if (!array1 || !array2) {
+    return null;
+  }
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-let isSameTree = (p, q) => {};
+let treeToArray = (tree) => {
+  let result = [];
+  if (!tree) {
+    return result;
+  }
+  result.push(tree.val);
+  if (tree.right) {
+    if (tree.left) {
+      let left = treeToArray(tree.left);
+      result.push(...left);
+    } else {
+      result.push(null);
+    }
+    let right = treeToArray(tree.right);
+    result.push(...right);
+  } else if (tree.left) {
+    let left = treeToArray(tree.left);
+    result.push(...left);
+  }
+  return result;
+};
 
-module.exports = { isSameTree, TreeNode };
+let isSameTree = (tree1, tree2) => {
+  return arraysAreAlike(treeToArray(tree1), treeToArray(tree2));
+};
+
+module.exports = { isSameTree, TreeNode, arraysAreAlike, treeToArray };
--- easy/tests/100.isSameTree.test.js
+++ easy/tests/100.isSameTree.test.js
@@ -24,38 +24,44 @@ describe("arraysAreAlike", () => {
 });
 
 describe("treeToArray", () => {
-  const example1A = TreeNode(1, TreeNode(2), TreeNode(3));
+  const example1A = new TreeNode(1, new TreeNode(2), new TreeNode(3));
   test("treeToArray(example1A) should be [1, 2, 3]", () => {
     expect(treeToArray(example1A)).toEqual([1, 2, 3]);
   });
 
-  const example2A = TreeNode(1, TreeNode(2));
+  const example2A = new TreeNode(1, new TreeNode(2));
   test("treeToArray(example2A) should be [1, 2]", () => {
     expect(treeToArray(example2A)).toEqual([1, 2]);
   });
 
-  const example2B = TreeNode(1, null, TreeNode(2));
-  test("treeToArray(example2B) should be [1, , 2]", () => {
-    expect(treeToArray(example2B)).toEqual([1, , 2]);
+  const example2B = new TreeNode(1, null, new TreeNode(2));
+  test("treeToArray(example2B) should be [1, null, 2]", () => {
+    expect(treeToArray(example2B)).toEqual([1, null, 2]);
   });
 });
 
 describe("isSameTree", () => {
-  const example1A = TreeNode(1, TreeNode(2), TreeNode(3));
-  const example1B = TreeNode(1, TreeNode(2), TreeNode(3));
+  const example1A = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+  const example1B = new TreeNode(1, new TreeNode(2), new TreeNode(3));
   test("isSameTree(example1A, example1B) should be true", () => {
     expect(isSameTree(example1A, example1B)).toBe(true);
   });
 
-  const example2A = TreeNode(1, TreeNode(2));
-  const example2B = TreeNode(1, TreeNode(null), TreeNode(2));
+  const example2A = new TreeNode(1, new TreeNode(2));
+  const example2B = new TreeNode(1, new TreeNode(null), new TreeNode(2));
   test("isSameTree(example2A, example2B) should be false", () => {
     expect(isSameTree(example2A, example2B)).toBe(false);
   });
 
-  const example3A = TreeNode(1, TreeNode(2), TreeNode(1));
-  const example3B = TreeNode(1, TreeNode(1), TreeNode(2));
+  const example3A = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+  const example3B = new TreeNode(1, new TreeNode(1), new TreeNode(2));
   test("isSameTree(example3A, example3B) should be false", () => {
     expect(isSameTree(example3A, example3B)).toBe(false);
+  });
+
+  const example4A = new TreeNode();
+  const example4B = new TreeNode();
+  test("isSameTree(example4A, example4B) should be true", () => {
+    expect(isSameTree(example4A, example4B)).toBe(true);
   });
 });
