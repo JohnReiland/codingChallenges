1c91849e6683f1a45fcc6567a035c0823ffb31fc
John Reiland <reiland.shanghai@gmail.com>
2020-10-13 19:05:44 -0400

    "Add solution for maxDepth"

--- easy/104.maxDepth.js
+++ easy/104.maxDepth.js
@@ -19,8 +19,22 @@ Given binary tree [3,9,20,null,null,15,7],
 return its depth = 3.
 */
 
-let maxDepth = (node) => {
-  
+/*
+This should be straightforward to accomplish with a recursive function.
+The max depth for a node is equal to the greater of the max depths of each of
+its children, plus one.
+*/
+
+let maxDepth = (node, depth = 0) => {
+  if (!node) {
+    return 0;
+  }
+  if (!node.left && !node.right) {
+    return 1;
+  }
+  let left = node.left ? maxDepth(node.left) : 0;
+  let right = node.right ? maxDepth(node.right) : 0;
+  return left > right ? left + 1 : right + 1;
 };
 
 function TreeNode(val, left, right) {
