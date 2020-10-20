e15d0dcb249804400a067001c1457d768726d03c
John Reiland <reiland.shanghai@gmail.com>
2020-10-20 11:36:02 -0400

    "Reneame test file"

--- easy/108.sortedArrayToBST.js
+++ easy/108.sortedArrayToBST.js
@@ -1,13 +1,16 @@
 /*
-Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+Given an array where elements are sorted in ascending order, convert it to a
+height balanced BST.
 
-For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+For this problem, a height-balanced binary tree is defined as a binary tree in
+which the depth of the two subtrees of every node never differ by more than 1.
 
 Example:
 
 Given the sorted array: [-10,-3,0,5,9],
 
-One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+One possible answer is: [0,-3,9,-10,null,5], which represents the following
+height balanced BST:
 
       0
      / \
@@ -16,7 +19,40 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
  -10  5
 */
 
-let sortedArrayToBST = (nums) => {};
+/*
+I'm not sure if I understand the challenge correctly, as it seems like it should
+be trivially easy. Take the middle-most index of the array (or one of the two
+middle-most in the event of an array with an even number of elements) and make
+that the root of the tree. For each index before it in the array, make its
+value the left leaf of the index after it. For each index after, make its
+value the right leaf of the index before it.
+
+Unless I'm misunderstanding the challenge, this should result in a valid
+solution.
+*/
+
+let sortedArrayToBST = (nums) => {
+  if (!nums) {
+    return;
+  }
+  let middlemostIndex = Math.floor(nums.length / 2);
+  let currentIndex = middlemostIndex - 1;
+  let root = new TreeNode(nums[middlemostIndex]);
+  let currentNode = root;
+  while (nums[currentIndex] !== undefined) {
+    currentNode.left = new TreeNode(nums[currentIndex]);
+    currentNode = currentNode.left;
+    currentIndex--;
+  }
+  currentIndex = middlemostIndex + 1;
+  currentNode = root;
+  while (nums[currentIndex] !== undefined) {
+    currentNode.right = new TreeNode(nums[currentIndex]);
+    currentNode = currentNode.right;
+    currentIndex++;
+  }
+  return root;
+};
 
 function TreeNode(val, left, right) {
   this.val = val === undefined ? 0 : val;
--- easy/tests/108.soretedArrayToBST.test.js
+++ easy/tests/108.soretedArrayToBST.test.js
@@ -4,9 +4,9 @@ describe("sortedArrayTOBST", () => {
   const tree1 = new TreeNode(
     0,
     new TreeNode(-3, new TreeNode(-10)),
-    new TreeNode(9, new TreeNode(5))
+    new TreeNode(5, new TreeNode(9))
   );
   test("sortedArrayToBST([-10, -3, 0, 5, 9]) should be tree1", () => {
-    expect(sortedArrayToBST([-10, -3, 0, 5, 9])).toBe(tree1);
+    expect(sortedArrayToBST([-10, -3, 0, 5, 9])).toStrictEqual(tree1);
   });
 });
