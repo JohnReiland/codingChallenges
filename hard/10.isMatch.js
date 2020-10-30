5089e8326587ccfe6db64d1d51eb95d4d5ddc2fc
John Reiland <reiland.shanghai@gmail.com>
2020-10-29 21:36:48 -0400

    "Add tests for isMatch"

--- hard/10.isMatch.js
+++ hard/10.isMatch.js
@@ -19,16 +19,10 @@ by repeating 'a' once, it becomes "aa".
 */
 
 /*
-Time complexity will be linear with respect to string, times linear with
-respect to patten, in a worse-case scenario. When the pattern fails to match on
-the first character, it's still a linear time complexity.
+Time complexity will be linear or worse with respect to string length, times
+linear or worse with respect to patten length.
 
-I don't see any way around looking at every character in the string and
-considering at each step whether the string is still a valid match for the
-pattern, as, depending on the pattern, a single char in the string that goes
-against the pattern can be enough to make it fail.
-
-I think different patterns require different search methods. Some possible
+Different patterns require different search methods. Some possible
 patterns, how they can be described with words, and how they could be searched:
 
 Pattern: .
@@ -37,11 +31,11 @@ Notes: length === 1
 
 Pattern: a
 Description: a single char, "a"
-Notes: length === 1, first char
+Notes: length === 1, first value === "a"
 
 Pattern: .a
 Description: any single char, followed by "a"
-Notes: length === 2, last value
+Notes: length === 2, last value === "a"
 
 Pattern: .. 
 Description: exactly two chars
@@ -49,43 +43,43 @@ Notes: length === 2
 
 Pattern: a.
 Description: "a", followed by any single char
-Notes: length === 2, first value
+Notes: length === 2, first value === "a"
 
 
 
 Pattern: aa
 Description: exactly two "a"s
-Notes: length === 2, first value, last value
+Notes: length === 2, first value === "a", last value === "a"
 
 Pattern: a.a
 Description: "a", followed by any single char, followed by "a"
-Notes: length === 3, first value, last value 
+Notes: length === 3, first value === "a", last value === "a"
 
 Pattern: a..a
 Description: "a", followed by exactly two chars, followed by "a"
-Notes: length === 4, first value, last value
+Notes: length === 4, first value === "a", last value === "a"
 
 
 
 Pattern: .a.
 Description: any single char, followed by "a", followed by any single char
-Notes: length === 3, second value
+Notes: length === 3, second value === "a"
 
 Pattern: .aa.
 Description: any single char, followed by exactly two "a"s, followed by any
 single char
-Notes: length === 4, second value, third value
+Notes: length === 4, second value === "a", third value === "a"
 
 Pattern: .a.a.
 Description: any single char, followed by "a", followed by any single char,
 followed by "a", followed by any single char
-Notes: length === 5, second value, forth value
+Notes: length === 5, second value === "a", forth value === "a"
 
 
 
 Pattern: a*
 Description: zero or more "a"s 
-Notes: every value until at least one non-"a"
+Notes: every value unless at least one non-"a"
 
 Pattern: .*
 Description: zero or more of any char
@@ -93,7 +87,7 @@ Notes: true
 
 Pattern: .*a
 Description: zero or more of any char, followed by "a"
-Notes: length >= 1, last value
+Notes: length >= 1, last value === "a"
 
 Pattern: .*.
 Description: zero or more of any char, followed by any single char
@@ -101,7 +95,7 @@ Notes: length >= 1
 
 Pattern: a.*
 Description: "a", followed by zero or more of any char
-Notes: length >= 1, first value
+Notes: length >= 1, first value === "a"
 
 Pattern: .*a.*
 Description: zero or more of any char, followed by "a", followed by zero or
@@ -119,7 +113,13 @@ Pattern: a*b.*
 Description: zero or more "a"s, followed by "b", followed by zero or more of
 any char
 Notes: length >= 1, every value until at least one non-"a", THEN, isMatch(b.*)
-on remainder of string (length >= 1, first char)
+on remainder of string (length >= 1, first char === "b")
+
+Pattern: a*.b.*
+Description: zero or more "a"s, followed by any single char ("a" inclusive!),
+followed by b, followed by zero of more of any char
+Notes: length >= 2, every value *starting with the second* until at least one
+"b"
 
 
 It's becoming clear that the first step in efficently determining if the string
@@ -137,28 +137,28 @@ but ..* cannot be simplified to just .*
 because ..* has a length of 1 or more, and .* has a length of 0 or more
 
 a*.*  and  .*a*  and  .*a*.*  can each be simplified to just .*
+.* could be said to absorb adjascent []* search components.
 
-This makes me feel that, when the pattern contains multiple [][]*, where the
-first [] is not ., that the solution will be recursive, searching the string for
-the first [], then invoking isMatch() on the rest of the string with the
-remaining unmatched part of the pattern. Whenever the end of the string is
-reached without the pattern being found, isMatch() returns false, and the search
-continues from the invokation above it, until the first invokation returns true
-or false.
+This makes me feel that the solution will be recursive, invoking isMatch() on
+portions of the original string with a portion the original pattern. Whenever
+the end of the string portion is reached without the pattern being matched,
+the current invokation of isMatch() returns false, which either results in the
+search continuing from the invokation above it or the top invokation returning
+false, depending on the nature of the pattern.
 
 Consider the pattern a.*b.*a
 Analizing the given pattern for shortcuts yields:
-length >=3, first char, last char
+length >= 3, first char, last char
 
 These initial tests all must pass. Any test failing results in a return of
 false.
 
 Assuming these initial tests all pass, isMatch() should then be called with
 the pattern  .*b.* , with instructions to begin after the first value of the
-string, end before the last value of the string, and that any time it fetches
-the length of the string, it should subtract 2 from it.
+string, end before the last value of the string, and with any time the length of
+the string being fetched, 2 being subtracted from it.
 Analizing this new pattern for shortcuts yields:
-length>=1, every char until at least one "b"
+length >= 1, every char until at least one "b"
 
 If these tests both pass, the string is a match. If either fails, it's not.
 
@@ -178,8 +178,98 @@ Analyzing this new pattern for shortcuts yeilds:
 length >= 1, every value until at least one "b"
 
 
+
+All this seems to suggest an outside-in approach to testing the string for a
+match to the pattern. A search pattern is first separated into an array of
+"search elements," which are either blocks of characters and/or dot (.)
+wildcards, length-unknowm runs of a known character, or length-unknown runs of
+any character(s).
+
+Thus, the search pattern ab*a.*ba*ca.*b
+would be separated into [a, b*, a, .*, b, a*, ca, .*, b].
+There are no []* elements adjacent to .* elements, so no absorbtion occurs.
+The sum of all fixed-length blocks is 6.
+First, the length would be tested to see if it is >= 6.
+Then first and last elements of the search element array would be tested.
+The first value of the string would be tested to see if it is "a".
+The last value of the string would be tested to see if it is "b".
+
+If all these tests pass, isMatch() would be called on what remains of the
+string, with the search element array [b*, a, .*, b, a*, ca, .*].
+The last element is .* and cannot be used to quickly rule out a string.
+The first element is b* and so the first non-"b" value can be tested further.
+
+The string would be tested until the first non-"b" value is found. isMatch()
+would be called on what remains of the stringt, with the search element array
+[a, .*, b, a*, ca, .*].
+The last element is .* and cannot be used to quickly rule out a string.
+The first value of the string would be tested to see if it is "a".
+
+If this test passes, isMatch() would be called on what remains of the string,
+with the search element array [.*, b, a*, ca, .*].
+.* at both ends of the search element array is kind of a worst-case scenario.
+It means every value of what remains of the string will need to be searched to
+rule out the possibility that a match exists, and if the searh element array
+contains []* values, then testing may need to resume on failures.
+In this example, the string would be searched until the first "b" is found.
+The location of this value would be noted.
+Then the first non-"a" value would would be tested to see if it is "c".
+IF it is,
+  then the next value would be tested to see if it is "a".
+ELSE
+  the search for "b" would resume from the previously noted location.
+
+HOWEVER: if there is no non-"a" value to test in what remains of the string,
+the search for "b" does NOT need to resume, and the top isMatch() call can
+return false.
+
+A distinction must be made, then, between a search failing because invalidating
+values have been encountered, and a search failing because the end of the search
+area has been reached. If ever the end of the search area is reached without
+every value getting an opportunity to be testsed, the string is certainly not a
+match.
+
+In short, .* on both ends of the search element array is bad, because then every
+index of the string must be touched at least once to prove there is no match,
+and if the array contains []* values besides, then one failure MAY no longer be 
+sufficent to prove the string is not a match, and indicies of the string MAY
+need to be touched multiple times.
+
+Also to be noted is the way dot (.) wildcards "distribute" into adjacent []*
+elements.
+Consider the search pattern a*.b.*
+it's not correct to test whether the second non-"a" value is "b", as "a" is a
+valid value for (.).
+Instead, the second value should be tested to see if it's "b". If so, the string
+passes.
+If not, THEN the second non-"a" value can be tested to see if it's "b" (this may
+end up testing the same value). If it's not, the string fails.
+
+How to describe this programatically:
+When search element array is [a*, .b] :
+If length of [a*] > 0, search for [b] || [.b]  (b OR .b)
+If length of [a*] === 0, search for [.b]
+
+When search element array is [a*, ..b] :
+If length of [a*] is >= 2, search for [b] || [.b] || [..b]  (b OR .b OR ..b)
+If length of [a*] === 1, search for [.b] || [..b]  (.b OR ..b)
+If length of [a*] === 0, search for [..b]
 */
 
-let isMatch = (string, pattern) => {};
+let isMatch = (string, pattern) => {
+  let patternElementArray = [];
+
+  // build function here to identify search pattern elements and push to array
+
+  let recurse = (
+    stringStart,
+    stringEnd,
+    patternStart,
+    patternEnd,
+    lengthAdjust
+  ) => {};
+
+  return recurse(0, string.length - 1, 0, patternElementArray.length - 1, 0);
+};
 
 module.exports = { isMatch };
--- hard/tests/10.isMatch.test.js
+++ hard/tests/10.isMatch.test.js
@@ -1,11 +1,259 @@
-const { isMAtch } = require("../10.isMatch");
+const { isMatch } = require("../10.isMatch");
 
-describe("isMatch()", () => {
-  test("isMatch(value) should be result", () => {
-    expect(isMAtch(value)).toBe(result);
+describe(`isMatch(".")`, () => {
+  test(`isMatch("", ".") should be false`, () => {
+    expect(isMatch("", ".")).toBe(false);
   });
 
-  test("isMatch(value) should be result", () => {
-    expect(isMAtch(value)).toBe(result);
+  test(`isMatch("aa", ".") should be false`, () => {
+    expect(isMatch("aa", ".")).toBe(false);
+  });
+
+  test(`isMatch("a", ".") should be true`, () => {
+    expect(isMatch("string", ".")).toBe(true);
+  });
+});
+
+describe(`isMatch("a")`, () => {
+  test(`isMatch("", "a") should be false`, () => {
+    expect(isMatch("", "a")).toBe(false);
+  });
+
+  test(`isMatch("b", "a") should be false`, () => {
+    expect(isMatch("b", "a")).toBe(false);
+  });
+
+  test(`isMatch("a", "a") should be true`, () => {
+    expect(isMatch("a", "a")).toBe(true);
+  });
+});
+
+describe(`isMatch(".a")`, () => {
+  test(`isMatch("a", ".a") should be false`, () => {
+    expect(isMatch("a", ".a")).toBe(false);
+  });
+
+  test(`isMatch("ab", ".a") should be false`, () => {
+    expect(isMatch("ab", ".a")).toBe(false);
+  });
+
+  test(`isMatch("ba", ".a") should be true`, () => {
+    expect(isMatch("ba", ".a")).toBe(true);
+  });
+});
+
+describe(`isMatch("..")`, () => {
+  test(`isMatch("a", "..") should be false`, () => {
+    expect(isMatch("a", "..")).toBe(false);
+  });
+
+  test(`isMatch("ab", "..") should be true`, () => {
+    expect(isMatch("ab", "..")).toBe(true);
+  });
+
+  test(`isMatch("ba", "a.") should be false`, () => {
+    expect(isMatch("ba", "a.")).toBe(false);
+  });
+
+  test(`isMatch("a", "a.") should be false`, () => {
+    expect(isMatch("a", "a.")).toBe(false);
+  });
+
+  test(`isMatch("a", "a.") should be false`, () => {
+    expect(isMatch("a", "a.")).toBe(false);
+  });
+
+  test(`isMatch("ab", "a.") should be true`, () => {
+    expect(isMatch("ab", "a.")).toBe(true);
+  });
+});
+
+describe(`isMatch("a.a")`, () => {
+  test(`isMatch("string", "a.a") should be false`, () => {
+    expect(isMatch("string", "a.a")).toBe(false);
+  });
+
+  test(`isMatch("string", "a.a") should be false`, () => {
+    expect(isMatch("string", "a.a")).toBe(false);
+  });
+
+  test(`isMatch("aa", "a.a") should be false`, () => {
+    expect(isMatch("aa", "a.a")).toBe(false);
+  });
+
+  test(`isMatch("aaa", "a.a") should be true`, () => {
+    expect(isMatch("aaa", "a.a")).toBe(true);
+  });
+});
+
+describe(`isMatch(".a.")`, () => {
+  test(`isMatch("aaaa", ".a.") should be false`, () => {
+    expect(isMatch("aaaa", ".a.")).toBe(false);
+  });
+
+  test(`isMatch("abc", ".a.") should be false`, () => {
+    expect(isMatch("abc", ".a.")).toBe(false);
+  });
+
+  test(`isMatch("bac", ".a.") should be true`, () => {
+    expect(isMatch("bac", ".a.")).toBe(true);
+  });
+});
+
+describe(`isMatch("a*")`, () => {
+  test(`isMatch("aaab", "a*") should be false`, () => {
+    expect(isMatch("aaab", "a*")).toBe(false);
+  });
+
+  test(`isMatch("aaaa", "a*") should be true`, () => {
+    expect(isMatch("aaaa", "a*")).toBe(true);
+  });
+
+  test(`isMatch("", "a*") should be true`, () => {
+    expect(isMatch("", "a*")).toBe(true);
+  });
+});
+
+describe(`isMatch(".*")`, () => {
+  test(`isMatch("", ".*") should be true`, () => {
+    expect(isMatch("", ".*")).toBe(true);
+  });
+
+  test(`isMatch("a", ".*") should be true`, () => {
+    expect(isMatch("a", ".*")).toBe(true);
+  });
+
+  test(`isMatch("abced", ".*") should be true`, () => {
+    expect(isMatch("abced", ".*")).toBe(true);
+  });
+});
+
+describe(`isMatch(".*a")`, () => {
+  test(`isMatch("", ".*a") should be false`, () => {
+    expect(isMatch("", ".*a")).toBe(false);
+  });
+
+  test(`isMatch("ab", ".*a") should be false`, () => {
+    expect(isMatch("ab", ".*a")).toBe(false);
+  });
+
+  test(`isMatch("a", ".*a") should be true`, () => {
+    expect(isMatch("a", ".*a")).toBe(true);
+  });
+
+  test(`isMatch("ba", ".*a") should be true`, () => {
+    expect(isMatch("ba", ".*a")).toBe(true);
+  });
+});
+
+describe(`isMatch(".*.")`, () => {
+  test(`isMatch("", ".*.") should be false`, () => {
+    expect(isMatch("", ".*.")).toBe(false);
+  });
+
+  test(`isMatch("a", ".*.") should be true`, () => {
+    expect(isMatch("a", ".*.")).toBe(true);
+  });
+
+  test(`isMatch("ab", ".*.") should be true`, () => {
+    expect(isMatch("ab", ".*.")).toBe(true);
+  });
+});
+
+describe(`isMatch(".*a.*")`, () => {
+  test(`isMatch("", ".*a.*") should be false`, () => {
+    expect(isMatch("", ".*a.*")).toBe(false);
+  });
+
+  test(`isMatch("bcdefgh", ".*a.*") should be false`, () => {
+    expect(isMatch("bcdefgh", ".*a.*")).toBe(false);
+  });
+
+  test(`isMatch("a", ".*a.*") should be true`, () => {
+    expect(isMatch("a", ".*a.*")).toBe(true);
+  });
+
+  test(`isMatch("abcdefgh", ".*a.*") should be true`, () => {
+    expect(isMatch("abcdefgh", ".*a.*")).toBe(true);
+  });
+
+  test(`isMatch("hgfedcba", ".*a.*") should be true`, () => {
+    expect(isMatch("hgfedcba", ".*a.*")).toBe(true);
+  });
+
+  test(`isMatch("hgfedcbabcdefgh", ".*a.*") should be true`, () => {
+    expect(isMatch("hgfedcbabcdefgh", ".*a.*")).toBe(true);
+  });
+});
+
+describe(`isMatch(".*..*")`, () => {
+  test(`isMatch("", ".*..*") should be false`, () => {
+    expect(isMatch("", ".*..*")).toBe(false);
+  });
+
+  test(`isMatch("a", ".*..*") should be true`, () => {
+    expect(isMatch("a", ".*..*")).toBe(true);
+  });
+
+  test(`isMatch("bc", ".*..*") should be true`, () => {
+    expect(isMatch("bc", ".*..*")).toBe(true);
+  });
+});
+
+describe(`isMatch("a*.b.*")`, () => {
+  test(`isMatch("b", "a*.b.*") should be false`, () => {
+    expect(isMatch("b", "a*.b.*")).toBe(false);
+  });
+
+  test(`isMatch("ab", "a*.b.*") should be true`, () => {
+    expect(isMatch("ab", "a*.b.*")).toBe(true);
+  });
+
+  test(`isMatch("aab", "a*.b.*") should be true`, () => {
+    expect(isMatch("aab", "a*.b.*")).toBe(true);
+  });
+
+  test(`isMatch("bb", "a*.b.*") should be true`, () => {
+    expect(isMatch("bb", "a*.b.*")).toBe(true);
+  });
+});
+
+describe(`isMatch("ab*a.*ba*ca.*b")`, () => {
+  test(`isMatch("aacab", "ab*a.*ba*ca.*b") should be false`, () => {
+    expect(isMatch("aacab", "ab*a.*ba*ca.*b")).toBe(false);
+  });
+
+  test(`isMatch("bbabcab", "ab*a.*ba*ca.*b") should be false`, () => {
+    expect(isMatch("bbabcab", "ab*a.*ba*ca.*b")).toBe(false);
+  });
+
+  test(`isMatch("aabaca", "ab*a.*ba*ca.*b") should be false`, () => {
+    expect(isMatch("aabaca", "ab*a.*ba*ca.*b")).toBe(false);
+  });
+
+  test(`isMatch("aacadb", "ab*a.*ba*ca.*b") should be false`, () => {
+    expect(isMatch("aacadb", "ab*a.*ba*ca.*b")).toBe(false);
+  });
+
+  test(`isMatch("aabdcab", "ab*a.*ba*ca.*b") should be false`, () => {
+    expect(isMatch("aabdcab", "ab*a.*ba*ca.*b")).toBe(false);
+  });
+
+  test(`isMatch("abbbcab", "ab*a.*ba*ca.*b") should be false`, () => {
+    expect(isMatch("abbbcab", "ab*a.*ba*ca.*b")).toBe(false);
+  });
+
+  test(`isMatch("aabcab", "ab*a.*ba*ca.*b") should be true`, () => {
+    expect(isMatch("aabcab", "ab*a.*ba*ca.*b")).toBe(true);
+  });
+
+  test(`isMatch("aabaadbaacab", "ab*a.*ba*ca.*b") should be true`, () => {
+    expect(isMatch("aabaadbaacab", "ab*a.*ba*ca.*b")).toBe(true);
+  });
+});
+
+describe(`isMatch("pattern")`, () => {
+  test(`isMatch("string", "pattern") should be result`, () => {
+    expect(isMatch("string", "pattern")).toBe(result);
   });
 });
