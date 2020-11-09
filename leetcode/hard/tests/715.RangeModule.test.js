3020eea42a1e7a9977a55b5cb015f57e667ca7fb
John Reiland <reiland.shanghai@gmail.com>
2020-11-09 18:05:28 -0500

    "Initial commit for RangeModule"

---
+++ leetcode/hard/tests/715.RangeModule.test.js
@@ -0,0 +1,7 @@
+const { RangeModule } = require("../715.RangeModule");
+
+describe.skip("functionName()", () => {
+  test("functionName(value)", () => {
+    expect(functionName(value)).toBe(result);
+  });
+});
