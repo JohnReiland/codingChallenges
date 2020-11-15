8049e81d674e7a405cb0db5dcfcdec9d09396338
John Reiland <reiland.shanghai@gmail.com>
2020-11-15 13:35:44 -0500

    "Initial commit for poorPigs"

---
+++ leetcode/hard/tests/458.poorPigs.test.js
@@ -0,0 +1,7 @@
+const {poorPigs} = require('../458.poorPigs');
+
+describe.skip('poorPigs()', () => {
+  test('poorPigs(buckets, minutesToDie, minutesToTest) should be result', () => {
+    expect(poorPigs(buckets, minutesToDie, minutesToTest)).toBe(result);
+  }
+})
