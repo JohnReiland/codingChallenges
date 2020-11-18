33ae5f46ab1cc6e1188b9ccc1ade5248fdc6c677
John Reiland <reiland.shanghai@gmail.com>
2020-11-18 05:27:13 -0500

    "Initial commit for merge"

---
+++ leetcode/medium/tests/56.merge.test.js
@@ -0,0 +1,7 @@
+const { merge } = require("../56.merge");
+
+describe("merge()", () => {
+  test("merge(value) should be result", () => {
+    expect(merge(value)).toStrictEqual(result);
+  });
+});
