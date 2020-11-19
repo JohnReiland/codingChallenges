cb46e7619ead5e7bb062658ba049ece3fc491562
John Reiland <reiland.shanghai@gmail.com>
2020-11-19 05:12:12 -0500

    "Initial commit for decodeString"

---
+++ leetcode/medium/tests/394.decodeString.test.js
@@ -0,0 +1,7 @@
+const { decodeSTring } = require("../394.decodeString");
+
+describe("decodeString()", () => {
+  test("decodeString(value) should be result", () => {
+    expect(decodeSTring(value)).toBe(result);
+  });
+});
