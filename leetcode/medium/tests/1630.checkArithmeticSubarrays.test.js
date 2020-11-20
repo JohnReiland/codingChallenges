0f63982b74c8d0a7bb4e665468651fb51c6faa57
John Reiland <reiland.shanghai@gmail.com>
2020-11-19 20:51:32 -0500

    "Initial commit for checkArithmeticSubarrays"

---
+++ leetcode/medium/tests/1630.checkArithmeticSubarrays.test.js
@@ -0,0 +1,11 @@
+const {
+  checkArithmeticSubarrays,
+} = require("../1630.checkArithmeticSubarrays");
+
+describe("checkArithmeticSubarrays()", () => {
+  test("checkArithmeticSubarrays(value1, value2, value3) should be result", () => {
+    expect(checkArithmeticSubarrays(value1, value2, value3)).toStrictEqual(
+      result
+    );
+  });
+});
