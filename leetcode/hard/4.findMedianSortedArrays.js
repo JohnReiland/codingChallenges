db50bd8806457731a5e28c50c759b366cd7ba29e
John Reiland <reiland.shanghai@gmail.com>
2020-11-21 19:01:00 -0500

    "Add solution for findMedianSortedArrays"

--- leetcode/hard/4.findMedianSortedArrays.js
+++ leetcode/hard/4.findMedianSortedArrays.js
@@ -1,42 +1,100 @@
 /*
-Given two sorted arrays nums1 and nums2 of size m and n respectively, return the
+Given two sorted arrays arr1 and arr2 of size m and n respectively, return the
 median of the two sorted arrays.
 
 Follow up: The overall run time complexity should be O(log (m+n)).
 
 
 Example 1:
-Input: nums1 = [1,3], nums2 = [2]
+Input: arr1 = [1,3], arr2 = [2]
 Output: 2.00000
 Explanation: merged array = [1,2,3] and median is 2.
 
 Example 2:
-Input: nums1 = [1,2], nums2 = [3,4]
+Input: arr1 = [1,2], arr2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 Example 3:
-Input: nums1 = [0,0], nums2 = [0,0]
+Input: arr1 = [0,0], arr2 = [0,0]
 Output: 0.00000
 
 Example 4:
-Input: nums1 = [], nums2 = [1]
+Input: arr1 = [], arr2 = [1]
 Output: 1.00000
 
 Example 5:
-Input: nums1 = [2], nums2 = []
+Input: arr1 = [2], arr2 = []
 Output: 2.00000
 
 Constraints:
 
-nums1.length == m
-nums2.length == n
+arr1.length == m
+arr2.length == n
 0 <= m <= 1000
 0 <= n <= 1000
 1 <= m + n <= 2000
--10^6 <= nums1[i], nums2[i] <= 10^6
+-10^6 <= arr1[i], arr2[i] <= 10^6
 */
 
-const findMedianSortedArrays = (nums1, nums2) => {};
+/*
+The follow up is key in finding the solution, I think. log (m + n) time means
+that with each step of processing the arrays, half the values should be
+discarded. Playing around with values, I found that the median of each array can
+be used as a guide to "safely" delete about half of the elements of the smaller
+array, and an equal number of elements from the larger array, without affecting
+the true result of their median. This reduces the size of the problem by half
+with each pass. 
+*/
 
-module.exports = { findMedianSortedArrays };
+const arrMedian = (array) => {
+  if (array.length % 2 === 0) {
+    const low = array.length / 2 - 1;
+    const high = array.length / 2;
+    return [(array[low] + array[high]) / 2, [low, high]];
+  }
+  const index = (array.length - 1) / 2;
+  return [array[index], [index]];
+};
+
+const findMedianSortedArrays = (arr1, arr2) => {
+  if (arr1.length < 3 || arr2.length < 3) {
+    const merged = [];
+    merged.push(...arr1);
+    merged.push(...arr2);
+    merged.sort((a, b) => a - b);
+    return arrMedian(merged)[0];
+  }
+  const larger = arr1.length >= arr2.length ? arr1 : arr2;
+  const smaller = arr1.length >= arr2.length ? arr2 : arr1;
+  const mLarge = arrMedian(larger);
+  const mSmall = arrMedian(smaller);
+  if (mLarge[0] === mSmall[0]) {
+    return mLarge[0];
+  }
+  if (
+    larger[0] >= smaller[smaller.length - 1] ||
+    smaller[0] >= larger[larger.length - 1]
+  ) {
+    if (larger.length === smaller.length) {
+      return larger[0] > smaller[smaller.length - 1]
+        ? arrMedian([smaller[smaller.length - 1], larger[0]])[0]
+        : arrMedian([larger[larger.length - 1], smaller[0]])[0];
+    }
+    return larger[0] >= smaller[smaller.length - 1]
+      ? arrMedian(larger.slice(0, larger.length - smaller.length))[0]
+      : arrMedian(larger.slice(smaller.length))[0];
+  }
+  const cut = mSmall[1][0];
+  return mLarge[0] > mSmall[0]
+    ? findMedianSortedArrays(
+        larger.slice(0, larger.length - cut),
+        smaller.slice(cut)
+      )
+    : findMedianSortedArrays(
+        larger.slice(cut),
+        smaller.slice(0, smaller.length - cut)
+      );
+};
+
+module.exports = { arrMedian, findMedianSortedArrays };
--- leetcode/hard/tests/4.findMedianSortedArrays.test.js
+++ leetcode/hard/tests/4.findMedianSortedArrays.test.js
@@ -1,12 +1,40 @@
-const { findMedianSortedArrays } = require("../4.findMedianSortedArrays");
+const {
+  arrMedian,
+  findMedianSortedArrays,
+} = require("../4.findMedianSortedArrays");
 
 describe("findMedianSortedArrays()", () => {
-  test("findMedianSortedArrays([1,3], [2]) shoule be 2", () => {
+  test("findMedianSortedArrays([1, 3], [2]) shoule be 2", () => {
     expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
   });
 
-  test("findMedianSortedArrays([1,2], [3,4]) shoule be 2.5", () => {
+  test("findMedianSortedArrays([1, 2], [3, 4]) shoule be 2.5", () => {
     expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
+  });
+
+  test("findMedianSortedArrays([1, 2], [5, 6, 7, 8]) shoule be 5.5", () => {
+    expect(findMedianSortedArrays([1, 2], [5, 6, 7, 8])).toBe(5.5);
+  });
+
+  test("findMedianSortedArrays([1, 2, 3], [5, 6, 7, 8]) shoule be 5", () => {
+    expect(findMedianSortedArrays([1, 2, 3], [5, 6, 7, 8])).toBe(5);
+  });
+
+  test("findMedianSortedArrays([1, 2, 3, 4], [6, 7, 8]) shoule be 4", () => {
+    expect(findMedianSortedArrays([1, 2, 3, 4], [6, 7, 8])).toBe(4);
+  });
+
+  test("findMedianSortedArrays([1, 2, 3, 20, 21, 22, 23, 24, 25], [0, 5, 10, 15, 20, 25, 30]) shoule be 20", () => {
+    expect(
+      findMedianSortedArrays(
+        [1, 2, 3, 20, 21, 22, 23, 24, 25],
+        [0, 5, 10, 15, 20, 25, 30]
+      )
+    ).toBe(20);
+  });
+
+  test("findMedianSortedArrays([1, 2], [1]) shoule be 1", () => {
+    expect(findMedianSortedArrays([1, 2], [1])).toBe(1);
   });
 
   test("findMedianSortedArrays([0,0], [0,0]) shoule be 0", () => {
@@ -23,5 +51,19 @@ describe("findMedianSortedArrays()", () => {
 
   test.skip("findMedianSortedArrays(array1, array2) shoule be result", () => {
     expect(findMedianSortedArrays(array1, array2)).toBe(result);
+  });
+});
+
+describe.skip("arrMedian()", () => {
+  test("arrMedian([1, 2, 3]) shoule be 2", () => {
+    expect(arrMedian([1, 2, 3])).toBe(2);
+  });
+
+  test("arrMedian([1, 2]) shoule be 1.5", () => {
+    expect(arrMedian([1, 2])).toBe(1.5);
+  });
+
+  test("arrMedian([1]) shoule be 1", () => {
+    expect(arrMedian([1])).toBe(1);
   });
 });
