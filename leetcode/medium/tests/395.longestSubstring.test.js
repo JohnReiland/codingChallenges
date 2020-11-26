58c3bf66aa2a3dbd38deefa926d5f3ee05d332be
John Reiland <reiland.shanghai@gmail.com>
2020-11-26 06:55:06 -0500

    "Initial commit for longestSubstring"

---
+++ leetcode/medium/tests/395.longestSubstring.test.js
@@ -0,0 +1,7 @@
+const { longestSubstring } = require("../395.longestSubstring");
+
+describe("longestSubstring()", () => {
+  test.skip(`longestSubstring("value") should be result`, () => {
+    expect(longestSubstring("value")).toBe(result);
+  });
+});
