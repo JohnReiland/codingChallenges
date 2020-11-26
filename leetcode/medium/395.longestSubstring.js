e1d6ccd72a6580a32d1073ecb7229e4c37dd97ef
John Reiland <reiland.shanghai@gmail.com>
2020-11-26 17:18:16 -0500

    "Add solution for longestSubstring"

--- leetcode/medium/395.longestSubstring.js
+++ leetcode/medium/395.longestSubstring.js
@@ -21,6 +21,66 @@ s consists of only lowercase English letters.
 1 <= k <= 10^5
 */
 
-const longestSubstring = (string, k) => {};
+/*
+Time complexity will be linear or worse, as every value must be touched, to know
+what character it is and how many times it occurs.
+
+I suspect I can get away with 2(n), one pass to identify characters that appear
+k or more times, a second pass to find the longest substring containing only
+those chars.
+
+UPDATE:
+I misunderstood the challenge. While the above produces valid results for the
+examples given, the substring itself must contain k or more of each character
+which makes up the substring. Thus, calling longestSubstring, on the longest
+substring, should return the length of the string.
+This might work as a solution, as each potential substring can be passed through
+a new call of longestSubstring. The value returned is the length of the largest
+valid substring in that potential substring. The largest value returned of all
+potential substrings is the length of the longest valid substring in the string.
+*/
+
+const longestSubstring = (string, k) => {
+  let result = 0;
+  if (k <= string.length) {
+    const counts = {};
+    const valid = new Set();
+    for (let i = 0; i < string.length; i++) {
+      counts[string[i]] =
+        counts[string[i]] === undefined ? 1 : counts[string[i]] + 1;
+      if (counts[string[i]] >= k) {
+        valid.add(string[i]);
+      }
+    }
+    if (valid.size === Object.keys(counts).length) {
+      return string.length;
+    }
+    let substringArray = [];
+    let currentSubstring = "";
+    for (let i = 0; i < string.length; i++) {
+      if (valid.has(string[i])) {
+        currentSubstring += string[i];
+      } else {
+        if (currentSubstring.length >= k) {
+          substringArray.push(currentSubstring);
+        }
+        currentSubstring = "";
+      }
+    }
+    if (currentSubstring.length >= k) {
+      substringArray.push(currentSubstring);
+    }
+
+    if (substringArray.length) {
+      let validSubstringLengths = substringArray.map((string) =>
+        longestSubstring(string, k)
+      );
+      result = validSubstringLengths.reduce(function (a, b) {
+        return Math.max(a, b);
+      });
+    }
+  }
+  return result;
+};
 
 module.exports = { longestSubstring };
--- leetcode/medium/tests/395.longestSubstring.test.js
+++ leetcode/medium/tests/395.longestSubstring.test.js
@@ -1,7 +1,44 @@
 const { longestSubstring } = require("../395.longestSubstring");
 
 describe("longestSubstring()", () => {
-  test.skip(`longestSubstring("value") should be result`, () => {
-    expect(longestSubstring("value")).toBe(result);
+  test(`longestSubstring("aaabb", 3) should be 3`, () => {
+    expect(longestSubstring("aaabb", 3)).toBe(3);
+  });
+
+  test(`longestSubstring("aaabb", 2) should be 5`, () => {
+    expect(longestSubstring("aaabb", 2)).toBe(5);
+  });
+
+  test(`longestSubstring("ababbc", 2) should be 5`, () => {
+    expect(longestSubstring("ababbc", 2)).toBe(5);
+  });
+
+  test(`longestSubstring("cababb", 2) should be 5`, () => {
+    expect(longestSubstring("cababb", 2)).toBe(5);
+  });
+
+  test(`longestSubstring("ababbc", 12) should be 0`, () => {
+    expect(longestSubstring("ababbc", 12)).toBe(0);
+  });
+
+  test(`longestSubstring("", 1) should be 0`, () => {
+    expect(longestSubstring("", 1)).toBe(0);
+  });
+
+  test(`longestSubstring("klidgfhkhbdfglibdshjwkjqaorjhnvgnjmfjhgavsdofgadlirgbidafvghydsufbge", 2) should be 17`, () => {
+    expect(
+      longestSubstring(
+        "klidgfhkhbdfglibdshjwkjqaorjhnvgnjmfjhgavsdofgadlirgbidafvghydsufbge",
+        2
+      )
+    ).toBe(17);
+  });
+
+  test(`longestSubstring("bchhbbdefghiaaacb", 3) should be 3`, () => {
+    expect(longestSubstring("bchhbbdefghiaaacb", 3)).toBe(3);
+  });
+
+  test.skip(`longestSubstring("value", kalue) should be result`, () => {
+    expect(longestSubstring("value", kalue)).toBe(result);
   });
 });
