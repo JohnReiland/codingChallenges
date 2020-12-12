bd73c5f99c801c1a65b90b1cff15600176c26ff3
John Reiland <reiland.shanghai@gmail.com>
2020-12-12 13:04:03 -0500

    "Add testMethods"

---
+++ testMethods.js
@@ -0,0 +1,19 @@
+Object.defineProperty(Array.prototype, "sortedToBST", {
+  value: function () {
+    if (!this.length) {
+      return null;
+    }
+    function TreeNode(val, left, right) {
+      this.val = val === undefined ? 0 : val;
+      this.left = left === undefined ? null : left;
+      this.right = right === undefined ? null : right;
+    }
+    const mid = Math.floor(this.length / 2);
+    const root = new TreeNode(this[mid]);
+    root.left = this.slice(0, mid).toBST();
+    root.right = this.slice(mid + 1, this.length).toBST();
+    return root;
+  },
+});
+
+module.exports = { sortedToBST };
