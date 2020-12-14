d8d74a1d92ccb88f88b9b1fe84a665e25f019ec4
John Reiland <reiland.shanghai@gmail.com>
2020-12-14 08:37:33 -0500

    "Save forward progress - maxCoins"

--- leetcode/hard/312.maxCoins.js
+++ leetcode/hard/312.maxCoins.js
@@ -20,6 +20,68 @@ burst them.
 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
 */
 
-const maxCoins = (nums) => {};
+const updateNode = (node) => {
+  node.gross =
+    node.val *
+    (node.left ? node.left.val : 1) *
+    (node.right ? node.right.val : 1);
+  node.costLeft =
+    (node.val > 0 ? node.val - 1 : 1) *
+    (node.left ? node.left.val : 1) *
+    (node.left && node.left.left ? node.left.left.val : 1);
+  node.costRight =
+    (node.val > 0 ? node.val - 1 : 1) *
+    (node.right ? node.right.val : 1) *
+    (node.right && node.right.right ? node.right.right.val : 1);
+  node.net = node.gross - (node.costLeft + node.costRight);
+  return;
+};
+
+const doublyLinkedList = (nums) => {
+  function Node(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+
+  let head = new Node(nums[0]);
+  let currentNode = head;
+  let result = [head];
+  for (let i = 1; i < nums.length; i++) {
+    currentNode.right = new Node(nums[i], currentNode);
+    currentNode = currentNode.right;
+    result.push(currentNode);
+  }
+  for (let i = 0; i < result.length; i++) {
+    updateNode(result[i]);
+  }
+
+  return result;
+};
+
+const maxCoins = (nums) => {
+  const list = doublyLinkedList(nums);
+  const value = [];
+  for (let i = 1; i < list.length - 1; i++) {
+    value.push(list[i]);
+  }
+
+  value.sort((a, b) => a.net + b.net);
+
+  let result = 0;
+  while (value.length) {
+    let currentNode = value.pop();
+    result += currentNode.gross;
+    currentNode.left.right = currentNode.right;
+    currentNode.right.left = currentNode.left;
+    updateNode(currentNode.left);
+    updateNode(currentNode.right);
+    value.sort((a, b) => a.net - b.net);
+  }
+
+  result += list[0].val * list[list.length - 1].val;
+  result += Math.max(list[0].val, list[list.length - 1].val);
+  return result;
+};
 
 module.exports = { maxCoins };
--- leetcode/hard/tests/312.maxCoins.test.js
+++ leetcode/hard/tests/312.maxCoins.test.js
@@ -1,12 +1,24 @@
 const { maxCoins } = require("../312.maxCoins");
 
 describe("maxCoins()", () => {
-  test("maxCoins([3,1,5,8]) should be 167", () => {
+  test("maxCoins([3, 1, 5, 8]) should be 167", () => {
     expect(maxCoins([3, 1, 5, 8])).toBe(167);
   });
 
-  test("maxCoins([8,5,4,3,2,6,1,4]) should be 724", () => {
-    expect(maxCoins([8, 5, 4, 3, 2, 6, 1, 4])).toBe(724);
+  test("maxCoins([8, 5, 3, 2, 6, 1, 4]) should be 622", () => {
+    expect(maxCoins([8, 5, 3, 2, 6, 1, 4])).toBe(622);
+  });
+
+  test("maxCoins([5, 3, 4, 2]) should be 115", () => {
+    expect(maxCoins([5, 3, 4, 2])).toBe(115);
+  });
+
+  test("maxCoins([5, 4, 3, 2]) should be 105", () => {
+    expect(maxCoins([5, 4, 3, 2])).toBe(105);
+  });
+
+  test("maxCoins([7, 6, 5, 4, 3, 4, 2, 7, 9]) should be 1424", () => {
+    expect(maxCoins([7, 6, 5, 4, 3, 4, 2, 7, 9])).toBe(1424);
   });
 
   test.skip("maxCoins(nums) should be result", () => {
