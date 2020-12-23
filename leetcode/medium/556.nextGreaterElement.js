2e6f2135ea17245d24b14656f869adb741af0d92
John Reiland <reiland.shanghai@gmail.com>
2020-12-23 10:16:46 -0500

    "Add solution for nextGreaterElement"

--- leetcode/medium/556.nextGreaterElement.js
+++ leetcode/medium/556.nextGreaterElement.js
@@ -7,17 +7,49 @@ Note that the returned integer should fit in 32-bit integer, if there is a valid
 answer but it does not fit in 32-bit integer, return -1.
 
 Example 1:
-Input: n = 12
+Input: int = 12
 Output: 21
 
 Example 2:
-Input: n = 21
+Input: int = 21
 Output: -1
 
 Constraints:
 1 <= n <= 2^31 - 1
 */
 
-const nextGreaterElement = (int) => {};
-
+const nextGreaterElement = (int) => {
+  let work = int;
+  let arr = [];
+  while (work !== 0) {
+    arr.push(work % 10);
+    work -= work % 10;
+    work /= 10;
+  }
+  let i = 1;
+  while (arr[i] >= arr[i - 1]) {
+    i++;
+  }
+  let result = -1;
+  if (i < arr.length) {
+    let partial = arr.slice(0, i + 1);
+    partial.sort((a, b) => a - b);
+    let j = 0;
+    while (partial[j] <= arr[i]) {
+      j++;
+    }
+    let head = partial[j];
+    partial.splice(j, 1);
+    for (let l = 0, k = partial.length - 1; k >= 0; l++, k--) {
+      arr[l] = partial[k];
+    }
+    arr[partial.length] = head;
+    result = 0;
+    while (arr.length) {
+      result *= 10;
+      result += arr.pop();
+    }
+  }
+  return result > 2147483647 ? -1 : result;
+};
 module.exports = { nextGreaterElement };
