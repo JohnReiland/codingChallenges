c35639d22dddca315073749ccb2d57a4017ff7b3
John Reiland <reiland.shanghai@gmail.com>
2020-12-23 07:24:45 -0500

    "Initial commit for nextGreaterElement"

---
+++ leetcode/medium/tests/556.nextGreaterElement.test.js
@@ -0,0 +1,15 @@
+const { nextGreaterElement } = require("../556.nextGreaterElement");
+
+describe("nextGreaterElement()", () => {
+  test("nextGreaterElement(12) should be 21", () => {
+    expect(nextGreaterElement(12)).toBe(21);
+  });
+
+  test("nextGreaterElement(21) should be -1", () => {
+    expect(nextGreaterElement(21)).toBe(-1);
+  });
+
+  test.skip("nextGreaterElement(int) should be result", () => {
+    expect(nextGreaterElement(int)).toBe(result);
+  });
+});
