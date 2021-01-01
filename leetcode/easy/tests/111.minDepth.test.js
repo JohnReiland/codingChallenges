ba49821895f96d6a0b569188a24941e9d128888c
John Reiland <reiland.shanghai@gmail.com>
2021-01-01 13:47:18 -0500

    "Initial commit for minDepth"

---
+++ leetcode/easy/tests/111.minDepth.test.js
@@ -0,0 +1,33 @@
+const { minDepth, TreeNode } = require("../111.minDepth");
+
+describe("minDepth()", () => {
+  const root1 = new TreeNode(
+    3,
+    new TreeNode(9),
+    new TreeNode(20, new TreeNode(15), new TreeNode(7))
+  );
+  test("minDepth(root1) should be 2", () => {
+    expect(minDepth(root1)).toBe(2);
+  });
+
+  const root2 = new TreeNode(
+    2,
+    new TreeNode(),
+    new TreeNode(
+      3,
+      new TreeNode(),
+      new TreeNode(
+        4,
+        new TreeNode(),
+        new TreeNode(5, new TreeNode(), new TreeNode(6))
+      )
+    )
+  );
+  test("minDepth(root2) should be 5", () => {
+    expect(minDepth(root2)).toBe(5);
+  });
+
+  test.skip("minDepth(root) should be result", () => {
+    expect(minDepth(root)).toBe(result);
+  });
+});
