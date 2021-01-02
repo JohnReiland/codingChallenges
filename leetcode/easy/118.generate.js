a7cfaa4b3e7fde9ad71b93521bae8ad6e766706f
John Reiland <reiland.shanghai@gmail.com>
2021-01-02 11:59:02 -0500

    "Add solution for generate"

--- leetcode/easy/118.generate.js
+++ leetcode/easy/118.generate.js
@@ -18,6 +18,33 @@ Output:
 ]
 */
 
-const generate = (numRows) => {};
+const generate = (numRows) => {
+  if (!generate.record) {
+    generate.record = [[], [1], [1, 1]];
+    generate.highest = 2;
+  }
+  let result = [];
+  if (numRows !== 0) {
+    while (generate.highest < numRows) {
+      let next = [1];
+      for (let i = 0; i < generate.record[generate.highest].length - 1; i++) {
+        next.push(
+          generate.record[generate.highest][i] +
+            generate.record[generate.highest][i + 1]
+        );
+      }
+      next.push(1);
+      generate.record.push(next);
+      generate.highest++;
+    }
+    result.push([1]);
+    let pushed = 2;
+    while (pushed <= numRows) {
+      result.push(generate.record[pushed]);
+      pushed++;
+    }
+  }
+  return result;
+};
 
 module.exports = { generate };
