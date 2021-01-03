cfb7ca987eb111a48310a787bae58f7341fbc772
John Reiland <reiland.shanghai@gmail.com>
2021-01-03 13:44:32 -0500

    "Inital commit for maxProfit"

---
+++ leetcode/easy/tests/121.maxProfit.test.js
@@ -0,0 +1,15 @@
+const { maxProfit } = require("../121.maxProfit");
+
+describe("maxProfit()", () => {
+  test("maxProfit([7, 1, 5, 3, 6, 4]) should be 5", () => {
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(5);
+  });
+
+  test("maxProfit([7, 6, 4, 3, 1]) should be 0", () => {
+    expect(maxProfit([7, 6, 4, 3, 1])).toBe(0);
+  });
+
+  test.skip("maxProfit(prices) should be result", () => {
+    expect(maxProfit(prices)).toBe(result);
+  });
+});
