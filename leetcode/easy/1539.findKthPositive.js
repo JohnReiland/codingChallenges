ae035bef09f7ff9197e98f2cf0bf6a437af4ac29
John Reiland <reiland.shanghai@gmail.com>
2021-01-06 13:50:31 -0500

    "Add solution for findKthPositive"

--- leetcode/easy/1539.findKthPositive.js
+++ leetcode/easy/1539.findKthPositive.js
@@ -24,6 +24,30 @@ Constraints:
 arr[i] < arr[j] for 1 <= i < j <= arr.length
 */
 
-const findKthPositive = (arr, target) => {};
+/*
+A brute force solution for this is very straightforward. With each element in
+arr, tally the number of missing positive integers. When that tally equals or
+exceeds k, count back from the last examined element until the kth missed
+integer is found. If the end of the array is encountered before the tally equals
+or exceeds k, count forward from the last examined element until the kth missed
+integer is found. Return the kth integer.
+
+A better solution exists which should be much faster. This solution invloves
+comparing array values to their indecies, allowing the element closest to the
+kth missing integer to be found without examining all or most elements in arr.
+With such tight input constraints, however, it's unclear whether the savings
+would be significant. I'll start with a brute force solution and do the more
+clever solution as a follow-up.
+*/
+
+const findKthPositive = (arr, target) => {
+  let missed = 0;
+  let last = 0;
+  for (let i = 0; missed < target && i < arr.length; i++) {
+    missed += arr[i] - 1 - last;
+    last = arr[i];
+  }
+  return last + (target >= missed ? target - missed : target - (missed + 1));
+};
 
 module.exports = { findKthPositive };
--- leetcode/easy/tests/1539.findKthPositive.test.js
+++ leetcode/easy/tests/1539.findKthPositive.test.js
@@ -5,7 +5,7 @@ describe("findKthPositive()", () => {
     expect(findKthPositive([2, 3, 4, 7, 11], 5)).toBe(9);
   });
 
-  test("findKthPositive([1,2,3,4], 2), should be 6", () => {
+  test("findKthPositive([1, 2, 3, 4], 2), should be 6", () => {
     expect(findKthPositive([1, 2, 3, 4], 2)).toBe(6);
   });
 
