c6f458208b4cc76ec5e9f8a0627d26f6d0d12190
John Reiland <reiland.shanghai@gmail.com>
2021-01-06 13:01:08 -0500

    "Initial commit for findKthPositive"

---
+++ leetcode/easy/tests/1539.findKthPositive.test.js
@@ -0,0 +1,15 @@
+const { findKthPositive } = require("../1539.findKthPositive");
+
+describe("findKthPositive()", () => {
+  test("findKthPositive([2, 3, 4, 7, 11], 5), should be 9", () => {
+    expect(findKthPositive([2, 3, 4, 7, 11], 5)).toBe(9);
+  });
+
+  test("findKthPositive([1,2,3,4], 2), should be 6", () => {
+    expect(findKthPositive([1, 2, 3, 4], 2)).toBe(6);
+  });
+
+  test.skip("findKthPositive(arr, target), should be result", () => {
+    expect(findKthPositive(arr, target)).toBe(result);
+  });
+});
