b9ff8a77362724dd54de32b88616f9cd2fed47ab
John Reiland <reiland.shanghai@gmail.com>
2021-01-09 22:38:16 -0500

    "Add solution for ladderLength"

--- leetcode/hard/127.ladderLength.js
+++ leetcode/hard/127.ladderLength.js
@@ -37,6 +37,97 @@ beginWord != endWord
 All the strings in wordList are unique.
 */
 
-const ladderLength = (beginWord, endWord, wordList) => {};
+/*
+I feel this is, in a way, a pathing problem. Starting in one location, I need to
+find the shortest route to another location. The locations are words, and the
+terrain is the wordList. Solving it as a pathing problem should be
+straightforword in design, if perhaps tricky in practice to build.
+For the startWord and each element in wordList, identify the shape of all
+psuedo-words it could transform to, and use these psuedo-words as properties
+in an object literal, with an array as the value and pushing the current index
+to that array. Lastly, and only for elements in wordList, test wether the
+current element is the endWord. If it is, set a target variable to that index.
+If that variable is -1 after all of wordList is processesd, set result to 0.
+Else, push any/all indicies from startWord's psuedo-words to a test array and
+set distance to 1.
+  While test array is not empty
+    Pop a value from the test array
+    if that value is the target value
+      set result to distance
+      break
+    else
+      push any/all indicies NOT ALREADY SEEN from that index value's
+      psuedo-words to a next array
+    if test array is empty
+      set test array to next array
+      set next array to empty array 
+    incremenet distance
+Return the result.
 
-module.exports = { ladderLength };
+*/
+
+const psuedoWords = (string) => {
+  const result = [];
+  for (let i = 0; i < string.length; i++) {
+    result.push(string.substr(0, i) + "*" + string.substr(i + 1));
+  }
+  return result;
+};
+
+const ladderLength = (beginWord, endWord, wordList) => {
+  const transformations = {};
+  let canTransform = false;
+  for (let i = 0; i < wordList.length; i++) {
+    canTransform = wordList[i] === endWord ? true : canTransform;
+    let temp = psuedoWords(wordList[i]);
+    for (let j = 0; j < temp.length; j++) {
+      if (!transformations[temp[j]]) {
+        transformations[temp[j]] = [];
+      }
+      transformations[temp[j]].push(i);
+    }
+  }
+  let result = 0;
+  if (canTransform) {
+    const startWords = psuedoWords(beginWord);
+    let seen = [];
+    let stack = [];
+    for (let i = 0; i < startWords.length; i++) {
+      if (transformations[startWords[i]]) {
+        for (let j = 0; j < transformations[startWords[i]].length; j++) {
+          if (seen[transformations[startWords[i]][j]] !== true) {
+            seen[transformations[startWords[i]][j]] = true;
+            stack.push(transformations[startWords[i]][j]);
+          }
+        }
+      }
+    }
+    let next = [];
+    let distance = 2;
+    while (stack.length) {
+      let currentWord = wordList[stack.pop()];
+      if (currentWord === endWord) {
+        result = distance;
+        break;
+      } else {
+        let temp = psuedoWords(currentWord);
+        for (let i = 0; i < temp.length; i++) {
+          for (let j = 0; j < transformations[temp[i]].length; j++) {
+            if (seen[transformations[temp[i]][j]] !== true) {
+              seen[transformations[temp[i]][j]] = true;
+              next.push(transformations[temp[i]][j]);
+            }
+          }
+        }
+      }
+      if (!stack.length) {
+        stack = next;
+        next = [];
+        distance++;
+      }
+    }
+  }
+  return result;
+};
+
+module.exports = { psuedoWords, ladderLength };
--- leetcode/hard/tests/127.ladderLength.test.js
+++ leetcode/hard/tests/127.ladderLength.test.js
@@ -1,6 +1,29 @@
-const { ladderLength } = require("../127.ladderLength");
+const { psuedoWords, ladderLength } = require("../127.ladderLength");
+
+describe("psuedoWords()", () => {
+  test(`psuedoWords("string") should be ["*tring", "s*ring", "st*ing", "str*ng", "stri*g", "strin*"]`, () => {
+    expect(psuedoWords("string")).toStrictEqual([
+      "*tring",
+      "s*ring",
+      "st*ing",
+      "str*ng",
+      "stri*g",
+      "strin*",
+    ]);
+  });
+
+  test.skip(`psuedoWords("string") should be result`, () => {
+    expect(psuedoWords("string")).toStrictEqual(result);
+  });
+});
 
 describe("ladderLength()", () => {
+  test(`ladderLength("hit", "hot", ["hot", "dot", "dog", "lot", "log", "cog"]) should be 2`, () => {
+    expect(
+      ladderLength("hit", "hot", ["hot", "dot", "dog", "lot", "log", "cog"])
+    ).toBe(2);
+  });
+
   test(`ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]) should be 5`, () => {
     expect(
       ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])
