1bac417c014f28b6ba852c652c7162b3c7ab6286
John Reiland <reiland.shanghai@gmail.com>
2021-01-11 10:20:50 -0500

    "Initial commit for merge (repeat)"

---
+++ leetcode/repeatChallenges/easy/88.merge.js
@@ -0,0 +1,28 @@
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one
+sorted array.
+
+The number of elements initialized in nums1 and nums2 are count1 and count2
+respectively.
+You may assume that nums1 has enough space (size that is equal to
+count1 + count2) to hold additional elements from nums2.
+
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], count1 = 3, nums2 = [2,5,6], count2 = 3
+Output: [1,2,2,3,5,6]
+
+Example 2:
+Input: nums1 = [1], count1 = 1, nums2 = [], count2 = 0
+Output: [1]
+
+Constraints:
+0 <= count1, count2 <= 200
+1 <= count1 + count2 <= 200
+nums1.length == count1 + count2
+nums2.length == count2
+-10^9 <= nums1[i], nums2[i] <= 10^9
+*/
+
+const merge = (nums1, count1, nums2, count2) => {};
+
+module.exports = { merge };
---
+++ leetcode/repeatChallenges/easy/tests/88.merge.test.js
@@ -0,0 +1,34 @@
+const { merge } = require("../88.merge");
+
+describe("merge()", () => {
+  const nums1A = [1, 2, 3, 0, 0, 0];
+  const nums2A = [2, 5, 6];
+  let count1A = 3;
+  let count2A = 3;
+  merge(nums1A, count1A, nums2A, count2A);
+  test("merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3) should be [1, 2, 2, 3, 5, 6]", () => {
+    expect(nums1A).toStrictEqual([1, 2, 2, 3, 5, 6]);
+  });
+
+  const nums1B = [1];
+  const nums2B = [];
+  let count1B = 1;
+  let count2B = 0;
+  merge(nums1B, count1B, nums2B, count2B);
+  test("merge([1], 1, [], 0) should be [1]", () => {
+    expect(nums1B).toStrictEqual([1]);
+  });
+
+  const nums1C = [];
+  const nums2C = [1];
+  let count1C = 0;
+  let count2C = 1;
+  merge(nums1C, count1C, nums2C, count2C);
+  test("merge([], 0, [1], 1) should be [1]", () => {
+    expect(nums1C).toStrictEqual([1]);
+  });
+
+  test.skip("merge(nums1, count1, nums2, count2) should be result", () => {
+    expect(merge(nums1, count1, nums2, count2)).toStrictEqual(result);
+  });
+});
