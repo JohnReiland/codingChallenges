ddb0eff7fa14d6fbf9d5abe0ac21ef27f1fa3b7c
John Reiland <reiland.shanghai@gmail.com>
2021-01-19 10:47:29 -0500

    "Add solution for longestPalindrome (repeat)"

--- leetcode/repeatChallenges/medium/5.longestPalindrome.js
+++ leetcode/repeatChallenges/medium/5.longestPalindrome.js
@@ -23,6 +23,51 @@ Constraints:
 s consist of only digits and English letters (lower-case and/or upper-case),
 */
 
-const longestPalindrome = (string) => {};
+const oddPalindromeAtInd = (string, index) => {
+  let result = 1;
+  let head = index - 1;
+  let foot = index + 1;
+  while (foot < string.length && head >= 0) {
+    if (string[head] !== string[foot]) {
+      break;
+    }
+    result += 2;
+    head--;
+    foot++;
+  }
+  return result;
+};
 
-module.exports = { longestPalindrome };
+const evenPalindromeAtInd = (string, index) => {
+  let result = 0;
+  let head = index;
+  let foot = index + 1;
+  while (foot < string.length && head >= 0) {
+    if (string[head] !== string[foot]) {
+      break;
+    }
+    result += 2;
+    head--;
+    foot++;
+  }
+  return result;
+};
+
+const longestPalindrome = (string) => {
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
+    let oddResult = oddPalindromeAtInd(string, i);
+    result =
+      oddResult > result.length
+        ? string.slice(i - (oddResult - 1) / 2, i + ((oddResult - 1) / 2 + 1))
+        : result;
+    let evenResult = evenPalindromeAtInd(string, i);
+    result =
+      evenResult > result.length
+        ? string.slice(i - (evenResult - 2) / 2, i + 1 + evenResult / 2)
+        : result;
+  }
+  return result;
+};
+
+module.exports = { oddPalindromeAtInd, evenPalindromeAtInd, longestPalindrome };
--- leetcode/repeatChallenges/medium/tests/5.longestPalindrome.test.js
+++ leetcode/repeatChallenges/medium/tests/5.longestPalindrome.test.js
@@ -1,4 +1,36 @@
-const { longestPalindrome } = require("../5.longestPalindrome");
+const {
+  oddPalindromeAtInd,
+  longestPalindrome,
+  evenPalindromeAtInd,
+} = require("../5.longestPalindrome");
+
+describe("oddPalindromeAtInd()", () => {
+  test(`oddPalindromeAtInd("babad", 0) should be 1`, () => {
+    expect(oddPalindromeAtInd("babad", 0)).toBe(1);
+  });
+
+  test(`oddPalindromeAtInd("babad", 1) should be 3`, () => {
+    expect(oddPalindromeAtInd("babad", 1)).toBe(3);
+  });
+
+  test.skip(`oddPalindromeAtInd("string", index) should be result`, () => {
+    expect(oddPalindromeAtInd("string", index)).toBe(result);
+  });
+});
+
+describe("evenPalindromeAtInd()", () => {
+  test(`evenPalindromeAtInd("cbbd", 0) should be 0`, () => {
+    expect(evenPalindromeAtInd("cbbd", 0)).toBe(0);
+  });
+
+  test(`evenPalindromeAtInd("cbbd", 1) should be 2`, () => {
+    expect(evenPalindromeAtInd("cbbd", 1)).toBe(2);
+  });
+
+  test.skip(`evenPalindromeAtInd("string", index) should be result`, () => {
+    expect(evenPalindromeAtInd("string", index)).toBe(result);
+  });
+});
 
 describe("longestPalindrome()", () => {
   test(`longestPalindrome("babad") should be "bab"`, () => {
