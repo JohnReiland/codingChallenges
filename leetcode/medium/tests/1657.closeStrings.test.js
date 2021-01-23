798ec36fd8ae046428d1f7d2a7156005c070f52d
John Reiland <reiland.shanghai@gmail.com>
2021-01-22 23:51:23 -0500

    "Initial commit for closeStrings"

---
+++ leetcode/medium/tests/1657.closeStrings.test.js
@@ -0,0 +1,23 @@
+const { closeStrings } = require("../1657.closeStrings");
+
+describe("closeStrings()", () => {
+  test(`closeStrings("abc", "bca") should be true`, () => {
+    expect(closeStrings("abc", "bca")).toBe(true);
+  });
+
+  test(`closeStrings("a", "aa") should be false`, () => {
+    expect(closeStrings("a", "aa")).toBe(false);
+  });
+
+  test(`closeStrings("cabbba", "abbccc") should be true`, () => {
+    expect(closeStrings("cabbba", "abbccc")).toBe(true);
+  });
+
+  test(`closeStrings("cabbba", "aabbss") should be false`, () => {
+    expect(closeStrings("cabbba", "aabbss")).toBe(false);
+  });
+
+  test.skip(`closeStrings("word1", "word2") should be result`, () => {
+    expect(closeStrings("word1", "word2")).toBe(result);
+  });
+});
