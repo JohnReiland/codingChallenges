5744c4f6ea25f45a27c18b88b94736b8368b7258
John Reiland <reiland.shanghai@gmail.com>
2021-01-24 09:51:57 -0500

    "Initial commit for mergeKLists"

---
+++ leetcode/hard/tests/23.mergeKLists.test.js
@@ -0,0 +1,34 @@
+const { ListNode, mergeKLists } = require("../23.mergeKLists");
+
+describe("mergeKLists()", () => {
+  const listsArr1 = [
+    [new ListNode(1, new ListNode(4, new ListNode(5)))],
+    [new ListNode(1, new ListNode(3, new ListNode(4)))],
+    [new ListNode(2, new ListNode(6))],
+  ];
+  const result1 = new ListNode(
+    1,
+    new ListNode(
+      1,
+      new ListNode(
+        2,
+        new ListNode(
+          3,
+          new ListNode(4, new ListNode(4, new ListNode(5, new ListNode(6))))
+        )
+      )
+    )
+  );
+  test("mergeKLists(listsArr1) should be result1", () => {
+    expect(mergeKLists(listsArr1)).toStrictEqual(result1);
+  });
+
+  const listsArr2 = [[]];
+  test("mergeKLists(listsArr1) should be null", () => {
+    expect(mergeKLists(listsArr1)).toStrictEqual(null);
+  });
+
+  test.skip("mergeKLists(listsArr) should be result", () => {
+    expect(mergeKLists(listsArr)).toStrictEqual(result);
+  });
+});
