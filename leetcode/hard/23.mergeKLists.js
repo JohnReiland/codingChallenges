25467278b94e2b6eae7ef27e05131ec9d931044b
John Reiland <reiland.shanghai@gmail.com>
2021-01-24 10:44:47 -0500

    "Add solution for mergeKLists"

--- leetcode/hard/23.mergeKLists.js
+++ leetcode/hard/23.mergeKLists.js
@@ -33,11 +33,49 @@ lists[i] is sorted in ascending order.
 The sum of lists[i].length won't exceed 10^4.
 */
 
+/*
+An idea occurs to me and it's the best kind, because I can't tell if it's silly
+or clever. The input will be an array of as many as 10,000 linked lists. It's
+not practical to find the lowest value among all lists for each step of building
+the result linked list. The range of values for each listNode, however, isn't
+that large. The val of each node could be used as a key to insert the node into
+an oject literal, with an array as the property of that key. When all lists have
+had all their nodes inserted in this way, the object is tested for every valid
+val, starting with -10^4 and going all the way to 10^4. Whenever a val works as
+a key, each element in its array is added, in order, as the next ListNode in the
+result array. When this is completed, the last ListNode has its next value set
+to null, and the result is returned.
+*/
+
 function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
 }
 
-const mergeKLists = (lists) => {};
+const mergeKLists = (lists) => {
+  const range = {};
+  const cap = new ListNode();
+  let resultNode = cap;
+  for (let i = 0; i < lists.length; i++) {
+    let currentNode = lists[i];
+    while (currentNode) {
+      if (!range[currentNode.val]) {
+        range[currentNode.val] = [];
+      }
+      range[currentNode.val].push(currentNode);
+      currentNode = currentNode.next;
+    }
+  }
+  for (let i = -10000; i <= 10000; i++) {
+    if (range[i]) {
+      for (let j = 0; j < range[i].length; j++) {
+        resultNode.next = range[i][j];
+        resultNode = resultNode.next;
+      }
+    }
+  }
+  resultNode.next = null;
+  return cap.next;
+};
 
 module.exports = { ListNode, mergeKLists };
--- leetcode/hard/tests/23.mergeKLists.test.js
+++ leetcode/hard/tests/23.mergeKLists.test.js
@@ -2,9 +2,9 @@ const { ListNode, mergeKLists } = require("../23.mergeKLists");
 
 describe("mergeKLists()", () => {
   const listsArr1 = [
-    [new ListNode(1, new ListNode(4, new ListNode(5)))],
-    [new ListNode(1, new ListNode(3, new ListNode(4)))],
-    [new ListNode(2, new ListNode(6))],
+    new ListNode(1, new ListNode(4, new ListNode(5))),
+    new ListNode(1, new ListNode(3, new ListNode(4))),
+    new ListNode(2, new ListNode(6)),
   ];
   const result1 = new ListNode(
     1,
@@ -23,9 +23,9 @@ describe("mergeKLists()", () => {
     expect(mergeKLists(listsArr1)).toStrictEqual(result1);
   });
 
-  const listsArr2 = [[]];
-  test("mergeKLists(listsArr1) should be null", () => {
-    expect(mergeKLists(listsArr1)).toStrictEqual(null);
+  const listsArr2 = [];
+  test("mergeKLists(listsArr2) should be null", () => {
+    expect(mergeKLists(listsArr2)).toStrictEqual(null);
   });
 
   test.skip("mergeKLists(listsArr) should be result", () => {
