da716c85ba8b8e8df56fab69b2ff3a164dbc7c03
John Reiland <reiland.shanghai@gmail.com>
2021-01-23 23:56:05 -0500

    "Add solution for diagonalSort"

--- leetcode/medium/1329.diagonalSort.js
+++ leetcode/medium/1329.diagonalSort.js
@@ -23,13 +23,58 @@ Output:
 ]
 
 Constraints:
-
 m == mat.length
 n == mat[i].length
 1 <= m, n <= 100
 1 <= mat[i][j] <= 100
 */
 
-const diagonalSort = (mat) => {};
+/*
+I don't work with matricies enough to know if there's a clever way to grab
+or assign values from/to multiple arrays faster than by nested iteration. As
+that's all I currently know, that's what I'll use, but I'll be interested to see
+if there's a better way I have yet to learn.
+*/
+
+const diagonalSort = (mat) => {
+  const m = mat.length;
+  const n = mat[0].length;
+  const asc = (a, b) => {
+    return a - b;
+  };
+  const flatten = (arr) => [].concat(...arr);
+  let diag = [];
+  const result = [];
+  for (let i = m - 1; i >= 0; i--) {
+    result.push([]);
+    let tmp = [];
+    for (let j = i, k = 0; j < m; j++, k++) {
+      tmp.push(mat[j][k]);
+    }
+    diag.push(tmp.sort(asc));
+  }
+  for (let i = 1; i < n; i++) {
+    let tmp = [];
+    for (let j = 0, k = i; j < m && k < n; j++, k++) {
+      tmp.push(mat[j][k]);
+    }
+    diag.push(tmp.sort(asc));
+  }
+  diag = flatten(diag);
+  let l = 0;
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = i, k = 0; j < m && k < n; j++, k++) {
+      result[j][k] = diag[l];
+      l++;
+    }
+  }
+  for (let i = 1; i < n; i++) {
+    for (let j = 0, k = i; j < m && k < n; j++, k++) {
+      result[j][k] = diag[l];
+      l++;
+    }
+  }
+  return result;
+};
 
 module.exports = { diagonalSort };
