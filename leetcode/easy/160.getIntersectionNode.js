da6d51a81347f78cdc6025cfbf10dc045c08cd58
John Reiland <reiland.shanghai@gmail.com>
2021-02-03 18:48:56 -0500

    Add improved solution for getIntersectionNode

--- leetcode/easy/160.getIntersectionNode.js
+++ leetcode/easy/160.getIntersectionNode.js
@@ -48,6 +48,31 @@ the Map, it would be returned. If none is found, null is returned.
 UPDATE:
 This solution passes, but isn't as fast of a solution as I expected. I'll study
 other solutions to learn new ways of looking at the challenge.
+
+UPDATE 2:
+Interesting! So, if the lists were always of the same length, it would be easy
+to find the intersection in linear time and constant memory, because you could
+use two pointers, one at the head of each list, and test whether they point to
+the same node. When they don't, both would be advanced until the intersection or
+end of the lists is found.
+
+This can't be done with the problem given, however, because the lists can be of
+different lengths, and so the pointers aren't guarenteed to arrive at the
+intersection at the same time. This is why I didn't use the described technique,
+but with cleverness, this obstacle can be overcome. Because two lists will, if
+they intersect, share any nodes after the intersection, while it can't be
+certain that they will both have the same number of nodes before any
+intersection, it can be certain they will both have the same number of nodes
+after it. By finding the length of both lists, the first node of the shorter
+list can be compared to a number of nodes of the longer list equal to the
+difference in their length. This is certain to balance any difference in the
+number of nodes prior to any intersection, and so after that number of nodes
+have been compared, both pointers can advance through their lists as originally
+described.
+
+The lesson to take from this is to consider whether there exist special cases of
+the challenge that would be much easier to solve, and whether all cases can be
+solved in a way to arrive at an equivilent form to that special case.
 */
 
 function ListNode(val, next = null) {
@@ -55,6 +80,7 @@ function ListNode(val, next = null) {
   this.next = next;
 }
 
+/*
 const getIntersectionNode = (headA, headB) => {
   const map = new Map();
   let result = null;
@@ -84,5 +110,38 @@ const getIntersectionNode = (headA, headB) => {
   }
   return result;
 };
+*/
+const listLength = (head) => {
+  let result = 0;
+  let node = head;
+  while (node) {
+    node = node.next;
+    result++;
+  }
+  return result;
+};
 
-module.exports = { ListNode, getIntersectionNode };
+const getIntersectionNode = (headA, headB) => {
+  let result = null;
+  if (headA && headB) {
+    let lenA = listLength(headA);
+    let lenB = listLength(headB);
+    let nodeA = headA;
+    let nodeB = headB;
+    while (nodeA) {
+      if (nodeA === nodeB) {
+        result = nodeA;
+        break;
+      }
+      [nodeA, nodeB, lenA, lenB] =
+        lenA === lenB
+          ? [nodeA.next, nodeB.next, lenA, lenB]
+          : lenA > lenB
+          ? [nodeA.next, nodeB, lenA - 1, lenB]
+          : [nodeA, nodeB.next, lenA, lenB - 1];
+    }
+  }
+  return result;
+};
+
+module.exports = { ListNode, listLength, getIntersectionNode };
