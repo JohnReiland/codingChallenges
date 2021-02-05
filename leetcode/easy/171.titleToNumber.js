d192221d6489b760e4c504af8ebbec1e1512d929
John Reiland <reiland.shanghai@gmail.com>
2021-02-05 11:50:05 -0500

    Add solution for titleToNumber

--- leetcode/easy/171.titleToNumber.js
+++ leetcode/easy/171.titleToNumber.js
@@ -30,6 +30,40 @@ string consists only of uppercase English letters.
 string is between "A" and "FXSHRXW".
 */
 
-const titleToNumber = (string) => {};
+const titleToNumber = (string) => {
+  const decode = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26,
+  };
+  let result = 0;
+  for (let i = 0; i < string.length; i++) {
+    result += decode[string[string.length - 1 - i]] * Math.pow(26, i);
+  }
+  return result;
+};
 
 module.exports = { titleToNumber };
