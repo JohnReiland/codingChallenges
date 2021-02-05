2e9c5edea28269e1172e7cfbbe954c08e0efa118
John Reiland <reiland.shanghai@gmail.com>
2021-02-05 00:18:05 -0500

    Initial commit for majorityElement

---
+++ leetcode/easy/tests/169.majorityElement.test.js
@@ -0,0 +1,15 @@
+const { majorityElement } = require("../169.majorityElement");
+
+describe("majorityElement()", () => {
+  test("majorityElement([3,2,3]) should be 3", () => {
+    expect(majorityElement([3, 2, 3])).toBe(3);
+  });
+
+  test("majorityElement([2,2,1,1,1,2,2]) should be 2", () => {
+    expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).toBe(2);
+  });
+
+  test.skip("majorityElement(nums) should be result", () => {
+    expect(majorityElement(nums)).toBe(result);
+  });
+});
