56d44af55c7c015eabc6927d7893efd39aab0da8
John Reiland <reiland.shanghai@gmail.com>
2021-02-13 23:33:58 -0500

    Add solution for shortestPathBinaryMatrix

--- leetcode/medium/1091.shortestPathBinaryMatrix.js
+++ leetcode/medium/1091.shortestPathBinaryMatrix.js
@@ -34,6 +34,75 @@ Note:
 grid[r][c] is 0 or 1
 */
 
-const shortestPathBinaryMatrix = (grid) => {};
+/*
+This one looks like fun, because it's pathing and pathing is always fun.
+I'm thinking build a helper function that takes one grid position and identifies
+all other grid positions that are valid moves from that first position, and
+which are not already marked as having been visited.
+Feeding the output of this function into its input results in an ordered search
+through a grid.
+
+Using this function both from the starting and ending points should result in a
+fast pathing solution while also being certain that the first path found is the
+shortest.
+*/
+
+const legalMoves = (grid, position, found) => {
+  let result = [];
+  for (let i = position[0] - 1; i <= position[0] + 1; i++) {
+    for (let j = position[1] - 1; j <= position[1] + 1; j++) {
+      if (
+        i >= 0 &&
+        i <= grid.length - 1 &&
+        j >= 0 &&
+        j <= grid.length - 1 &&
+        grid[i][j] === 0 &&
+        !found[i][j]
+      ) {
+        result.push([i, j]);
+      }
+    }
+  }
+  return result;
+};
+
+const shortestPathBinaryMatrix = (grid) => {
+  let result = -1;
+  if (grid[0][0] === 0) {
+    const startFound = new Array(grid.length);
+    for (let i = 0; i < startFound.length; i++) {
+      startFound[i] = new Array(grid.length);
+    }
+    startFound[0][0] = true;
+    let startStack = [[0, 0]];
+    let next = [];
+    let count = 2;
+    if (grid.length === 1 && grid[0][0] === 0) {
+      result = 1;
+    }
+    while (startStack.length && result < 0) {
+      let currentPosition = startStack.pop();
+      let newfound = legalMoves(grid, currentPosition, startFound);
+      for (let i = 0; i < newfound.length; i++) {
+        if (
+          newfound[i][0] === grid.length - 1 &&
+          newfound[i][1] === grid.length - 1
+        ) {
+          result = count;
+          break;
+        } else {
+          startFound[newfound[i][0]][newfound[i][1]] = true;
+        }
+      }
+      next.push(...newfound);
+      if (!startStack.length) {
+        startStack = next;
+        next = [];
+        count++;
+      }
+    }
+  }
+  return result;
+};
 
 module.exports = { shortestPathBinaryMatrix };
--- leetcode/medium/tests/1091.shortestPathBinaryMatrix.test.js
+++ leetcode/medium/tests/1091.shortestPathBinaryMatrix.test.js
@@ -2,6 +2,25 @@ const {
   shortestPathBinaryMatrix,
 } = require("../1091.shortestPathBinaryMatrix");
 
+describe.skip("legalMoves()", () => {
+  const position1 = [0, 0];
+  const visited1 = [];
+  const result1 = [[0, 1]];
+  test("legalMoves(position1, visited1) should be result1", () => {
+    expect(legalMoves(position1, visited1)).toStrictEqual(result1);
+  });
+
+  const position2 = [0, 1];
+  const visited2 = [[true]];
+  const result2 = [
+    [0, 2],
+    [1, 2],
+  ];
+  test("legalMoves(position2, visited2) should be result2", () => {
+    expect(legalMoves(position2, visited2)).toStrictEqual(result2);
+  });
+});
+
 describe("shortestPathBinaryMatrix()", () => {
   const grid1 = [
     [0, 1],
