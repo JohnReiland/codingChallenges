2fa0286594a3c840c97b41e2df172a736d40426d
John Reiland <reiland.shanghai@gmail.com>
2021-02-16 23:33:35 -0500

    Initial commit for letterCasePermutation

---
+++ leetcode/medium/tests/784.letterCasePermutation.test.js
@@ -0,0 +1,38 @@
+const { letterCasePermutation } = require("../784.letterCasePermutation");
+
+describe("letterCasePermutation()", () => {
+  test(`letterCasePermutation("string") should be result`, () => {
+    expect(letterCasePermutation("string")).toEqual(
+      expect.arrayContaining(result)
+    );
+    expect(letterCasePermutation("string").length).toBe(result.length);
+  });
+
+  test(`letterCasePermutation("string") should be result`, () => {
+    expect(letterCasePermutation("string")).toEqual(
+      expect.arrayContaining(result)
+    );
+    expect(letterCasePermutation("string").length).toBe(result.length);
+  });
+
+  test(`letterCasePermutation("string") should be result`, () => {
+    expect(letterCasePermutation("string")).toEqual(
+      expect.arrayContaining(result)
+    );
+    expect(letterCasePermutation("string").length).toBe(result.length);
+  });
+
+  test(`letterCasePermutation("string") should be result`, () => {
+    expect(letterCasePermutation("string")).toEqual(
+      expect.arrayContaining(result)
+    );
+    expect(letterCasePermutation("string").length).toBe(result.length);
+  });
+
+  test.skip(`letterCasePermutation("string") should be result`, () => {
+    expect(letterCasePermutation("string")).toEqual(
+      expect.arrayContaining(result)
+    );
+    expect(letterCasePermutation("string").length).toBe(result.length);
+  });
+});
