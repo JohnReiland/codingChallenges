2aad681d1523424e48a613aab2896a6e506a9e01
John Reiland <reiland.shanghai@gmail.com>
2021-02-17 20:55:49 -0500

    Add letterCasePermutation solut. : speed > 99.57%!

--- leetcode/medium/784.letterCasePermutation.js
+++ leetcode/medium/784.letterCasePermutation.js
@@ -26,7 +26,36 @@ S will be a string with length between 1 and 12.
 S will consist only of letters or digits.
 */
 
-const letterCasePermutation = (string, result = []) => {
+const letterCasePermutation = (string) => {
+  let result = [];
+  if (string.length) {
+    let index = 0;
+    let cap = "";
+    while (!isNaN(parseInt(string[index]))) {
+      cap += string[index];
+      index++;
+    }
+    let letter;
+    if (index < string.length) {
+      letter = string[index].toLowerCase();
+    }
+    const tail = letterCasePermutation(string.slice(index + 1));
+    if (!tail.length) {
+      if (letter) {
+        result.push(cap + (letter || ""));
+        result.push(cap + (letter ? letter.toUpperCase() : ""));
+      } else {
+        result.push(cap);
+      }
+    } else if (letter) {
+      for (let i = 0; i < tail.length; i++) {
+        result.push(cap + (letter || "") + tail[i]);
+        result.push(cap + (letter ? letter.toUpperCase() : "") + tail[i]);
+      }
+    } else {
+      result = tail;
+    }
+  }
   return result;
 };
 
