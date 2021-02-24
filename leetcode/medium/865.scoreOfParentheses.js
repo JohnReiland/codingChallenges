80cd5ad718566695f882c95dd27743134a564086
John Reiland <reiland.shanghai@gmail.com>
2021-02-24 11:20:53 -0500

    Add solution for scoreOfParentheses

--- leetcode/medium/865.scoreOfParentheses.js
+++ leetcode/medium/865.scoreOfParentheses.js
@@ -28,6 +28,19 @@ string is a balanced parentheses string, containing only ( and ).
 2 <= string.length <= 50
 */
 
-const scoreOfParentheses = (string) => {};
+const scoreOfParentheses = (string) => {
+  let result = 0;
+  let count = 1;
+  let left = 1;
+  for (let i = 1; i < string.length; i++) {
+    count = string[i] === ")" ? count - 1 : count + 1;
+    if (!count) {
+      let inner = scoreOfParentheses(string.slice(left, i));
+      result += 2 * inner;
+      left = i + 2;
+    }
+  }
+  return result || 0.5;
+};
 
 module.exports = { scoreOfParentheses };
