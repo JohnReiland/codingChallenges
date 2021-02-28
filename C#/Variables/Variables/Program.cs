51fee50036ee167ece3f27bff3022f044b032477
John Reiland <reiland.shanghai@gmail.com>
2021-02-28 08:33:03 -0500

    Add C# folder

---
+++ C#/Variables/Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string exampleString = "true";
+                bool exampleBoolean = Convert.ToBoolean(exampleString);
+                Console.WriteLine(exampleBoolean);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The string could not be converted to a boolean.");
+                throw;
+            }
+        }
+    }
+}
